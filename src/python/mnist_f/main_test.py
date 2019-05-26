b6551bfc2f3e59fa13a9667a9d8194f2fb8c5b21
Daniel J. Beutel <daniel.beutel@xain.io>
2019-05-26 20:30:08 +0200

    Add tests for mnist_f training set split

--- src/python/mnist_f/main_test.py
+++ src/python/mnist_f/main_test.py
@@ -1,3 +1,4 @@
+import numpy as np
 from mnist_f import main
 
 
@@ -7,3 +8,30 @@ def test_load():
   assert x_test.shape[0] == y_test.shape[0]
   assert len(x_train.shape) == len(x_test.shape)
   assert len(y_train.shape) == len(y_test.shape)
+
+
+def test_split_num_splits():
+  # Prepare
+  x = np.zeros((3,28,28))
+  y = np.zeros((3))
+  num_splits = 3
+  # Execute
+  x_splits, y_splits = main.split(x, y, num_splits)
+  # Assert
+  assert len(x_splits) == num_splits
+  assert len(y_splits) == num_splits
+  # By the transitive property of == also:
+  # len(x_splits) == len(y_splits)
+
+
+def test_split_dims():
+  # Prepare
+  x = np.zeros((3,28,28))
+  y = np.zeros((3))
+  num_splits = 3
+  # Execute
+  x_splits, y_splits = main.split(x, y, num_splits)
+  # Assert: Corresponding x and y have the same number of examples
+  for xs, ys in zip(x_splits, y_splits):
+    assert xs.shape[0] == ys.shape[0]
+  # TODO Assert: Each split has the same dimensionality (except for number of examples)
