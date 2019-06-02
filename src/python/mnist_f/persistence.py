1393a3bcb0d7d7d8658da8425a9f389ad410e323
Daniel J. Beutel <daniel.beutel@xain.io>
2019-06-02 20:43:59 +0200

    Add MNIST-F persistence stub

---
+++ src/python/mnist_f/persistence.py
@@ -0,0 +1,26 @@
+import glob
+from typing import List
+
+import numpy as np
+
+
+def store_shards(shards: List[np.ndarray]):
+  for i, shard in enumerate(shards):
+    store(shard, fname="mnist_shard_" + str(i))
+
+
+def load_shards(dir: str) -> List[np.ndarray]:
+  shards = []
+  for fname in glob.glob("mnist_shard_*.npy"):
+    shard = load(fname)
+    shards.append(shard)
+  return shards
+
+
+def store(data: np.ndarray, fname: str) -> bool:
+  data[0][1][1][0] = 254
+  np.save(fname, data)
+
+
+def load(fname: str) -> np.ndarray:
+  return np.load(fname)
---
+++ src/python/mnist_f/persistence_test.py
@@ -0,0 +1,19 @@
+import numpy as np
+from mnist_f import persistence
+
+
+# TODO mark as integration test
+def test_store_load():
+  fname = "a"
+  fname_full = "a.npy"
+  
+  # Create NumPy array
+  a_expected = np.zeros(shape=(3,28,28,1), dtype=np.uint8)
+  a_expected[0][1][1][0] = 255
+
+  # Store to disk, then load from disk
+  persistence.store(a_expected, fname=fname)
+  a_actual = persistence.load(fname=fname_full)
+
+  # Test equality
+  assert np.array_equal(a_expected, a_actual)
