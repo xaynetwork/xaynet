b875697df9fa9830d01660566b00ef0a1a205145
Daniel J. Beutel <daniel.beutel@xain.io>
2019-06-04 19:13:38 +0200

    Introduce participant abstraction and use it to train individual models on individual data partitions

--- autofl/fedml/fedml.py
+++ autofl/fedml/fedml.py
@@ -1,3 +1,6 @@
+from typing import Tuple
+import numpy as np
+from numpy import ndarray
 import tensorflow as tf
 from autofl.mnist_f import mnist_f
 from autofl.fedml import net
@@ -13,13 +16,37 @@ def main():
     # Create model
     model = net.fc()
     model.summary()
-    # Train model using only one partition
-    x_train = x_splits[0]
-    y_train = y_splits[0]
-    x_train, x_test = x_train / 255.0, x_test / 255.0
-    model.fit(x_train, y_train, epochs=5)
-    model.evaluate(x_test, y_test)
+    # Train independent models on each data partition
+    ps = []
+    for x_split, y_split in zip(x_splits, y_splits):
+        model = net.fc()  # Create a new model for each participant
+        participant = Participant(model, x_split, y_split)
+        ps.append(participant)
+    # Train each model
+    for p in ps:
+        p.train(epochs=2)
+    # Evaluate the individual performance of each model
+    for i, p in enumerate(ps):
+        loss, accuracy = p.evaluate(x_test, y_test)
+        print("Participant", i, ":", loss, accuracy)
 
 
-def integer_addition(x: int, y: int) -> int:
-    return x + y
+class Participant:
+    def __init__(
+        self, model: tf.keras.Model, x_split: ndarray, y_split: ndarray
+    ) -> None:
+        assert x_split.shape[0] == y_split.shape[0]
+        self.model = model
+        self.x_split = x_split
+        self.y_split = y_split
+        self.history = None
+
+    def train(self, epochs: int):
+        x_train = self.x_split / 255.0
+        y_train = self.y_split
+        self.history = self.model.fit(x_train, y_train, epochs=epochs)
+
+    def evaluate(self, x_test: ndarray, y_test: ndarray) -> Tuple[float, float]:
+        x_test = x_test / 255.0
+        loss, accuracy = self.model.evaluate(x_test, y_test)
+        return loss, accuracy
--- autofl/fedml/fedml_test.py
+++ autofl/fedml/fedml_test.py
@@ -1,7 +1,26 @@
+import numpy as np
 from autofl.fedml import fedml
 
 
-def test_integer_addition():
-    expected = 3
-    actual = fedml.integer_addition(1, 2)
-    assert expected == actual
+def test_Participant_x_y_shape_valid():
+    # Prepare
+    m = None
+    x = np.zeros((3, 28, 28))
+    y = np.zeros((3))
+    # Execute
+    _ = fedml.Participant(m, x, y)
+    # Assert
+    pass
+
+
+def test_Participant_x_y_shape_invalid():
+    # Prepare
+    m = None
+    x = np.zeros((3, 28, 28))
+    y = np.zeros((4))
+    # Execute & assert
+    try:
+        _ = fedml.Participant(m, x, y)
+        fail()
+    except:
+        pass
