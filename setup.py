161cf41e1e6e88c0113f261f96ae8dd49afc4f06
Daniel J. Beutel <daniel.beutel@xain.io>
2019-06-04 12:04:48 +0200

    Change dependency Pillow-SIMD to Pillow, install specific pip version during setup (19.1.1)

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ install_requires = [
     "tensorflow-datasets==1.0.2",
     "tensorflow-federated==0.4.0",
     "matplotlib==3.0.3",
-    "Pillow-SIMD==5.3.0.post1",
+    "Pillow==6.0.0",
 ]
 
 cpu_require = ["tensorflow==1.13.1"]
