0d2817ca42ca4974ebdcba26c182c65002de59ec
Taner Topal <tanertopal42@gmail.com>
2019-06-06 15:20:57 +0200

    Add scripts to set git hooks

---
+++ scripts/set_git_hooks.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+PRE_PUSH_HOOK="./.git/hooks/pre-push"
+
+cd $DIR/../
+
+if [ -f "$PRE_PUSH_HOOK" ]; then
+    echo "$PRE_PUSH_HOOK exist"
+else 
+    echo "#!/bin/sh" >> $PRE_PUSH_HOOK
+    echo "./scripts/test.sh" >> $PRE_PUSH_HOOK
+    
+    chmod +x $PRE_PUSH_HOOK
+
+    echo "$PRE_PUSH_HOOK created"
+fi
