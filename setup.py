0f3d0e35c09ca0b96d1fbb4f4fe0b06bbce9b531
Daniel J. Beutel <daniel.beutel@xain.io>
2019-06-11 17:33:38 +0200

    Add absl-py as direct dependency

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ if sys.version_info < (3, 6):
 install_requires = [
     "typing-extensions==3.7.2",
     "numpy==1.16.3",
+    "absl-py==0.7.1",
     "tensorflow-datasets==1.0.2",
     "tensorflow-federated==0.4.0",
     "matplotlib==3.0.3",
