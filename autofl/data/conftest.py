8a4fb39fca7d534af65cb1c7c41fe389c76d1e40
Taner Topal <tanertopal42@gmail.com>
2019-06-12 16:41:24 +0200

    Reduce number of possible side effects for tests

--- autofl/data/conftest.py
+++ autofl/data/conftest.py
@@ -1,14 +1,46 @@
+from typing import Tuple
+
 import pytest
 import tensorflow as tf
+import numpy as np
+
+from .data import load
+
+
+class keras_dataset:
+    @staticmethod
+    def load_data() -> Tuple[
+        Tuple[np.ndarray, np.ndarray], Tuple[np.ndarray, np.ndarray]
+    ]:
+        labels = np.arange(10, dtype=np.int8)
+
+        x_train = np.ones((60, 32, 32, 3), dtype=np.int8)
+        y_train = np.tile(labels, 6)
+
+        x_test = np.ones((10, 32, 32, 3), dtype=np.int8)
+        y_test = np.tile(labels, 1)
+
+        return (x_train, y_train), (x_test, y_test)
 
 
 @pytest.fixture
-def dataset_mnist():
-    (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
-    return (x_train, y_train, x_test, y_test)
+def dataset():
+    (x_train, y_train, x_test, y_test) = load(keras_dataset)
 
+    assert isinstance(
+        x_train, np.ndarray
+    ), "load method in data.py seems to be out of sync with assumptions in fixture"
 
-@pytest.fixture
-def dataset_cifar10():
-    (x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data()
+    assert isinstance(
+        y_train, np.ndarray
+    ), "load method in data.py seems to be out of sync with assumptions in fixture"
+
+    assert isinstance(
+        x_test, np.ndarray
+    ), "load method in data.py seems to be out of sync with assumptions in fixture"
+
+    assert isinstance(
+        x_test, np.ndarray
+    ), "load method in data.py seems to be out of sync with assumptions in fixture"
+
     return (x_train, y_train, x_test, y_test)
--- autofl/data/stats_test.py
+++ autofl/data/stats_test.py
@@ -1,49 +1,21 @@
 from autofl.data.stats import basic_statistics
 
 
-def test_basic_statistics_with_default_mnist(dataset_mnist):
-    (x_train, y_train, x_test, y_test) = dataset_mnist
-
-    # Making sure our dataset is correct
-    assert x_train.shape[0] == y_train.shape[0]
-    assert x_test.shape[0] == y_test.shape[0]
-
-    stats = basic_statistics(dataset_mnist)
+def test_basic_statistics(dataset):
+    stats = basic_statistics(dataset)
 
     assert isinstance(stats, dict)
     assert isinstance(stats["train"], dict)
     assert isinstance(stats["test"], dict)
 
-    assert stats["train"]["number_of_examples"] == 60000
+    assert stats["train"]["number_of_examples"] == 60
+    assert stats["test"]["number_of_examples"] == 10
 
     assert len(stats["train"]["number_of_examples_per_label"][0]) == 10
-
-    print(stats["train"]["number_of_examples_per_label"])
-
-    for count in stats["train"]["number_of_examples_per_label"][1]:
-        # not all labels are euqally distributed but definitly more than 5k times in cifar10
-        assert count > 5000
-
-
-def test_basic_statistics_with_default_cifar10(dataset_cifar10):
-    (x_train, y_train, x_test, y_test) = dataset_cifar10
-
-    # Making sure our dataset is correct
-    assert x_train.shape[0] == y_train.shape[0]
-    assert x_test.shape[0] == y_test.shape[0]
-
-    stats = basic_statistics(dataset_cifar10)
-
-    assert isinstance(stats, dict)
-    assert isinstance(stats["train"], dict)
-    assert isinstance(stats["test"], dict)
-
-    assert stats["train"]["number_of_examples"] == 50000
-
-    assert len(stats["train"]["number_of_examples_per_label"][0]) == 10
-
-    print(stats["train"]["number_of_examples_per_label"])
+    assert len(stats["test"]["number_of_examples_per_label"][0]) == 10
 
     for count in stats["train"]["number_of_examples_per_label"][1]:
-        # each label will ocur 5k times in cifar10
-        assert count == 5000
+        assert count == 6
+
+    for count in stats["test"]["number_of_examples_per_label"][1]:
+        assert count == 1
