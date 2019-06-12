3e523a253f9e83add3c14dec7051895578cdf294
Taner Topal <tanertopal42@gmail.com>
2019-06-12 18:38:01 +0200

    Restructure for better IDE discoverability

--- autofl/data/stats.py
+++ autofl/data/stats.py
@@ -1,37 +1,21 @@
-from collections.abc import Mapping
 from typing import List, Tuple
 
 import numpy as np
 
 
-class DatasetStats(Mapping):
+class DatasetStats:
     """
     Inheriting from Mapping makes DatasetStats immutable after instantiation
     See: https://docs.python.org/3/library/collections.abc.html
     """
 
-    def __init__(self, *args, **kw):
-        # Make certain properties required
-        assert "number_of_examples" in kw, "property number_of_examples is required"
-
-        assert (
-            "number_of_examples_per_label" in kw
-        ), "property number_of_examples_per_label is required"
+    def __init__(self, number_of_examples: int, number_of_examples_per_label: tuple):
+        self.number_of_examples = number_of_examples
+        self.number_of_examples_per_label = number_of_examples_per_label
 
         assert isinstance(
-            kw["number_of_examples_per_label"], tuple
+            number_of_examples_per_label, tuple
         ), "property number_of_examples_per_label should be a tuple"
-
-        self._storage = dict(*args, **kw)
-
-    def __getitem__(self, key):
-        return self._storage[key]
-
-    def __iter__(self):
-        return iter(self._storage)
-
-    def __len__(self):
-        return len(self._storage)
 
 
 def basic_stats(dataset: Tuple[np.ndarray, np.ndarray]) -> DatasetStats:
--- autofl/data/stats_test.py
+++ autofl/data/stats_test.py
@@ -1,12 +1,6 @@
 import numpy as np
-import pytest
 
 from autofl.data.stats import DatasetStats, basic_stats, basic_stats_multiple
-
-
-@pytest.mark.xfail(strict=True)
-def test_create_dataset_stats_failure():
-    DatasetStats()
 
 
 def test_create_dataset_stats():
@@ -18,8 +12,8 @@ def test_create_dataset_stats():
         number_of_examples_per_label=number_of_examples_per_label,
     )
 
-    assert stats["number_of_examples"] == 1
-    assert stats["number_of_examples_per_label"] == number_of_examples_per_label
+    assert stats.number_of_examples == 1
+    assert stats.number_of_examples_per_label == number_of_examples_per_label
 
 
 def test_basic_stats(dataset):
@@ -27,13 +21,11 @@ def test_basic_stats(dataset):
     stats = basic_stats((x, y))
 
     assert isinstance(stats, DatasetStats)
-    assert "number_of_examples" in stats
-    assert "number_of_examples_per_label" in stats
 
-    assert stats["number_of_examples"] == 60
-    assert len(stats["number_of_examples_per_label"][0]) == 10
+    assert stats.number_of_examples == 60
+    assert len(stats.number_of_examples_per_label[0]) == 10
 
-    for count in stats["number_of_examples_per_label"][1]:
+    for count in stats.number_of_examples_per_label[1]:
         assert count == 6
 
 
@@ -43,5 +35,3 @@ def test_basic_stats_multiple(dataset):
 
     for stats in stats_list:
         assert isinstance(stats, DatasetStats)
-        assert "number_of_examples" in stats
-        assert "number_of_examples_per_label" in stats
