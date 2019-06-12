5f77703a1c4fe59f56281c950eaff989ee313456
Taner Topal <tanertopal42@gmail.com>
2019-06-12 17:48:01 +0200

    Improve stats by introducing DatasetStat class

--- autofl/data/stats.py
+++ autofl/data/stats.py
@@ -1,9 +1,35 @@
+from collections.abc import Mapping
 from typing import Dict, List, Tuple
 
 import numpy as np
 
 
-def basic_stats(dataset: Tuple[np.ndarray, np.ndarray]) -> Dict:
+class DatasetStats(Mapping):
+    """
+    Inheriting from Mapping makes DatasetStats immutable after instantiation
+    See: https://docs.python.org/3/library/collections.abc.html
+    """
+
+    def __init__(self, *args, **kw):
+        # Make certain properties required
+        assert "number_of_examples" in kw, "property number_of_examples is required"
+        assert (
+            "number_of_examples_per_label" in kw
+        ), "property number_of_examples_per_label is required"
+
+        self._storage = dict(*args, **kw)
+
+    def __getitem__(self, key):
+        return self._storage[key]
+
+    def __iter__(self):
+        return iter(self._storage)
+
+    def __len__(self):
+        return len(self._storage)
+
+
+def basic_stats(dataset: Tuple[np.ndarray, np.ndarray]) -> DatasetStats:
     """
     Creates dataset statistics for a dataset of the shape:
 
@@ -16,10 +42,10 @@ def basic_stats(dataset: Tuple[np.ndarray, np.ndarray]) -> Dict:
 
     (x, y) = dataset
 
-    return {
-        "number_of_examples": x.shape[0],
-        "number_of_examples_per_label": np.unique(y, return_counts=True),
-    }
+    return DatasetStats(
+        number_of_examples=x.shape[0],
+        number_of_examples_per_label=np.unique(y, return_counts=True),
+    )
 
 
 def basic_stats_multiple(datasets: List[Tuple[np.ndarray, np.ndarray]]) -> List[Dict]:
--- autofl/data/stats_test.py
+++ autofl/data/stats_test.py
@@ -1,10 +1,11 @@
-from autofl.data.stats import basic_stats, basic_stats_multiple
+from autofl.data.stats import DatasetStats, basic_stats, basic_stats_multiple
 
 
 def test_basic_stats(dataset):
     (x, y, _, _) = dataset
     stats = basic_stats((x, y))
 
+    assert isinstance(stats, DatasetStats)
     assert "number_of_examples" in stats
     assert "number_of_examples_per_label" in stats
 
@@ -20,5 +21,6 @@ def test_basic_stats_multiple(dataset):
     stats_list = basic_stats_multiple([(x1, y1), (x2, y2)])
 
     for stat in stats_list:
+        assert isinstance(stat, DatasetStats)
         assert "number_of_examples" in stat
         assert "number_of_examples_per_label" in stat
