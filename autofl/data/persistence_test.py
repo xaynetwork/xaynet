2c109731a81222d3abbc344c08b34da78e87738d
Taner Topal <tanertopal42@gmail.com>
2019-06-12 12:35:55 +0200

    Move tmp file into /tmp

--- autofl/data/persistence_test.py
+++ autofl/data/persistence_test.py
@@ -8,16 +8,15 @@ from autofl.data import persistence
 # TODO find a better solution for a.npy
 @pytest.mark.integration
 def test_store_load():
-    fname = "a"
-    fname_full = "a.npy"
+    tmp_file = "/tmp/a.npy"
 
     # Create NumPy array
     a_expected = np.zeros(shape=(3, 28, 28, 1), dtype=np.uint8)
     a_expected[0][1][1][0] = 255
 
     # Store to disk, then load from disk
-    persistence.store(a_expected, fname=fname)
-    a_actual = persistence.load(fname=fname_full)
+    persistence.store(a_expected, fname=tmp_file)
+    a_actual = persistence.load(fname=tmp_file)
 
     # Test equality
     assert np.array_equal(a_expected, a_actual)
