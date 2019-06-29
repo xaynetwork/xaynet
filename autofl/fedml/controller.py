6c4cd4c7a02404527f3bfd409a390a6ba4f50cad
Daniel J. Beutel <daniel.beutel@xain.io>
2019-06-29 14:13:00 +0200

    Add CycleRandomController, refactor RandomController and RoundRobinController

--- autofl/fedml/controller.py
+++ autofl/fedml/controller.py
@@ -1,15 +1,18 @@
 import random
+from abc import ABC
+from typing import List
+
+import numpy as np
 
 from .ops import federated_averaging
 
 
-class RandomController:
-    def __init__(self, num_participants: int, C: int):
+class Controller(ABC):
+    def __init__(self, num_participants) -> None:
         self.num_participants = num_participants
-        self.C = C
 
-    def indices(self):
-        return random.sample(range(0, self.num_participants), self.C)
+    def indices(self) -> List[int]:
+        raise NotImplementedError("not implemented")
 
     @staticmethod
     def aggregate(thetas):
@@ -17,6 +20,34 @@ class RandomController:
         return theta_prime
 
 
-class RoundRobinController(RandomController):
+class RandomController(Controller):
+    def __init__(self, num_participants: int, C: int):
+        super().__init__(num_participants)
+        self.C = C
+
+    def indices(self) -> List[int]:
+        return random.sample(range(0, self.num_participants), self.C)
+
+
+class RoundRobinController(Controller):
     def __init__(self, num_participants: int) -> None:
-        super().__init__(num_participants, C=1)
+        super().__init__(num_participants)
+        self.next_index: int = 0
+
+    def indices(self) -> List[int]:
+        next_index = self.next_index
+        self.next_index = (next_index + 1) % self.num_participants
+        return [next_index]
+
+
+class CycleRandomController(Controller):
+    def __init__(self, num_participants: int) -> None:
+        super().__init__(num_participants)
+        self.cycle: np.ndarray = np.array([])
+
+    def indices(self):
+        if self.cycle.size == 0:
+            self.cycle = np.random.permutation(self.num_participants)
+        next_index = self.cycle[0]
+        self.cycle = self.cycle[1:]
+        return next_index
