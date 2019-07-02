ad50a5eeed8f04a6efc481765e833d8e7015284b
Taner Topal <tanertopal42@gmail.com>
2019-07-02 12:16:51 +0200

    Add split and test split loading methods

--- autofl/data/persistence.py
+++ autofl/data/persistence.py
@@ -146,7 +146,14 @@ def download_remote_ndarray(
     split_name: str,
     local_datasets_dir: str,
 ) -> Optional[FederatedDatasetSplit]:
-    """Downloads dataset split and loads from disk if already present"""
+    """Downloads dataset split and loads from disk if already present
+
+    Parameters:
+    datasets_repository (str): datasets repository base URL
+    dataset_name (str): Name of dataset in repository
+    split_name (str): Split name. Example: "x0.npy"
+    local_datasets_dir (str): Directory in which all local datasets are stored
+    """
     url = "{}/{}/{}".format(datasets_repository, dataset_name, split_name)
 
     dataset_dir = get_dataset_dir(dataset_name, local_datasets_dir)
--- autofl/datasets/cifar10_random_splits_10_test.py
+++ autofl/datasets/cifar10_random_splits_10_test.py
@@ -1,7 +1,10 @@
+import os
+
 import numpy as np
 import tensorflow as tf
 
-from autofl.datasets import cifar10_random_splits_10
+import autofl.datasets.cifar10_random_splits_10 as ds
+from autofl.data import persistence
 
 
 def test_generate_dataset(mock_keras_dataset, monkeypatch):
@@ -9,7 +12,7 @@ def test_generate_dataset(mock_keras_dataset, monkeypatch):
     monkeypatch.setattr(tf.keras.datasets, "cifar10", mock_keras_dataset)
 
     # Execute
-    xy_splits, xy_test = cifar10_random_splits_10.generate_dataset()
+    xy_splits, xy_test = ds.generate_dataset()
 
     # Assert
     # -> Verify types
@@ -34,9 +37,70 @@ def test_generate_dataset(mock_keras_dataset, monkeypatch):
     assert y_test.shape == (10,)
 
 
-def test_store_dataset():
-    # TODO
+def test_load_split(monkeypatch, tmp_path):
     # Prepare
+    split_index = 5
+    xy_expected = (np.ones((3, 2)), np.ones((3)))
+    x_expected, y_expected = xy_expected
+
+    def mock_download_remote_ndarray(
+        datasets_repository: str,
+        dataset_name: str,
+        split_name: str,
+        local_datasets_dir: str,
+    ):
+        # Check if split_name contains the right index
+        # Assert: Check if local_datasets_dir was correctly passed through
+        assert local_datasets_dir == tmp_path
+
+        if split_name == "x{}.npy".format(split_index):
+            return x_expected
+        if split_name == "y{}.npy".format(split_index):
+            return y_expected
+
+        raise Exception("split_name was incorrect")
+
+    monkeypatch.setattr(
+        persistence, "download_remote_ndarray", mock_download_remote_ndarray
+    )
+
     # Execute
+    x_actual, y_actual = ds.load_split(split_index, local_datasets_dir=tmp_path)
+
     # Assert
-    pass
+    np.testing.assert_equal(x_actual, x_actual)
+    np.testing.assert_equal(y_actual, y_actual)
+
+
+def test_load_test(monkeypatch, tmp_path):
+    # Prepare
+    xy_expected = (np.ones((3, 2)), np.ones((3)))
+    x_expected, y_expected = xy_expected
+
+    def mock_download_remote_ndarray(
+        datasets_repository: str,
+        dataset_name: str,
+        split_name: str,
+        local_datasets_dir: str,
+    ):
+        # Check if split_name contains the right index
+        # Assert: Check if local_datasets_dir was correctly passed through
+        assert local_datasets_dir == tmp_path
+
+        if split_name == "x_test.npy":
+            return x_expected
+        if split_name == "y_test.npy":
+            return y_expected
+
+        raise Exception("split_name was incorrect")
+
+    monkeypatch.setattr(
+        persistence, "download_remote_ndarray", mock_download_remote_ndarray
+    )
+
+    # Execute
+    x_actual, y_actual = ds.load_test(local_datasets_dir=tmp_path)
+
+    # Assert
+    np.testing.assert_equal(x_actual, x_actual)
+    np.testing.assert_equal(y_actual, y_actual)
