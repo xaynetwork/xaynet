83f6ec40dce6c30abe7ec9b87eeb04df0c1ca1ad
Taner Topal <tanertopal42@gmail.com>
2019-07-02 18:27:17 +0200

    Dataset loading

--- autofl/datasets/cifar10_random_splits_10_test.py
+++ autofl/datasets/cifar10_random_splits_10_test.py
@@ -1,39 +1,74 @@
+from typing import Tuple
+
 import numpy as np
 
-from . import cifar10_random_splits_10 as ds
+from . import cifar10_random_splits_10 as cifar10
 from . import storage
+
+
+def test_load_splits(monkeypatch):
+    # Prepare
+    split_ids_expected = [str(i).zfill(2) for i in range(10)] + ["test"]
+    split_ids_actual = []
+
+    def mock_load_split(split_id, split_hashes):
+        split_ids_actual.append(split_id)
+
+        assert isinstance(split_hashes, tuple)
+
+        # Will will later check if load_splits will return
+        # these tupples in a FederatedDataset structure
+        return (None, None)
+
+    monkeypatch.setattr(cifar10, "load_split", mock_load_split)
+
+    # Execute
+    xy_splits_actual, xy_test_actual = cifar10.load_splits()
+
+    # Assert
+    assert split_ids_expected == split_ids_actual
+
+    for xy_actual in xy_splits_actual:
+        assert (None, None) == xy_actual
+
+    assert (None, None) == xy_test_actual
 
 
 def test_load_split(monkeypatch, tmp_path):
     # Prepare
-    split_id = "05"
-    xy_expected = (np.ones((3, 2)), np.ones((3)))
-    x_expected, y_expected = xy_expected
+    split_id_expected = "05"
+    split_hashes_expected = ("foo", "bar")
+    x_expected = np.ones((3, 2))
+    y_expected = np.ones((3))
 
-    def mock_download_remote_ndarray(
+    def mock_load_split(
         datasets_repository: str,
         dataset_name: str,
-        split_name: str,
+        split_id: str,
+        split_hashes: Tuple[str, str],
         local_datasets_dir: str,
     ):
-        # Check if split_name contains the right index
-        # Assert: Check if local_datasets_dir was correctly passed through
+        # Assert: avoid linter warnings so checking some defaults
+        assert isinstance(datasets_repository, str)
+        assert dataset_name == cifar10.DATASET_NAME
+
+        # Assert: Check if arguments ar passed through correctly
+        assert split_id == split_id_expected
+        assert split_hashes == split_hashes_expected
         assert local_datasets_dir == tmp_path
 
-        if split_name == "x_{}.npy".format(split_id):
-            return x_expected
-        if split_name == "y_{}.npy".format(split_id):
-            return y_expected
+        return x_expected, y_expected
 
-        raise Exception("split_name was incorrect")
-
-    monkeypatch.setattr(
-        storage, "download_remote_ndarray", mock_download_remote_ndarray
-    )
+    # We are mocking the load_split method in the storage module
+    # which will be called by the load_split method in the cifar10 module
+    monkeypatch.setattr(storage, "load_split", mock_load_split)
 
     # Execute
-    x_actual, y_actual = ds.load_split(split_id, local_datasets_dir=tmp_path)
+    x_actual, y_actual = cifar10.load_split(
+        split_id=split_id_expected,
+        split_hashes=split_hashes_expected,
+        local_datasets_dir=tmp_path,
+    )
 
-    # Assert
-    np.testing.assert_equal(x_actual, x_actual)
-    np.testing.assert_equal(y_actual, y_actual)
+    np.testing.assert_equal(x_actual, x_expected)
+    np.testing.assert_equal(y_actual, y_expected)
--- autofl/datasets/storage.py
+++ autofl/datasets/storage.py
@@ -1,10 +1,23 @@
+import hashlib
 import os
 import shutil
+from typing import Tuple
 
 import numpy
 import requests
 
-from ..types import FederatedDatasetSplit
+
+def sha1checksum(fpath: str):
+    sha1 = hashlib.sha1()
+
+    with open(fpath, "rb") as f:
+        while True:
+            data = f.read()
+            if not data:
+                break
+            sha1.update(data)
+
+    return sha1.hexdigest()
 
 
 def get_dataset_dir(dataset_name: str, local_datasets_dir: str) -> str:
@@ -17,31 +30,73 @@ def get_dataset_dir(dataset_name: str, local_datasets_dir: str) -> str:
     return dataset_dir
 
 
-def download_remote_ndarray(
+def fetch_ndarray(url, fpath):
+    """Get file from fpath and store at fpath"""
+    response = requests.get(url, stream=True)
+
+    with open(fpath, "wb") as fin:
+        shutil.copyfileobj(response.raw, fin)
+
+
+def load_ndarray(
     datasets_repository: str,
     dataset_name: str,
-    split_name: str,
+    ndarray_name: str,
+    ndarray_hash: str,
     local_datasets_dir: str,
-) -> FederatedDatasetSplit:
-    """Downloads dataset split and loads from disk if already present
+):
+    """Downloads dataset ndarray and loads from disk if already present
 
     Parameters:
     datasets_repository (str): datasets repository base URL
     dataset_name (str): Name of dataset in repository
-    split_name (str): Split name. Example: "x0.npy"
+    ndarray_name (str): ndarray name. Example: "x0.npy"
     local_datasets_dir (str): Directory in which all local datasets are stored
     """
-    url = "{}/{}/{}".format(datasets_repository, dataset_name, split_name)
+    url = "{}/{}/{}".format(datasets_repository, dataset_name, ndarray_name)
 
     dataset_dir = get_dataset_dir(dataset_name, local_datasets_dir)
-    fpath = os.path.join(dataset_dir, split_name)
+    fpath = os.path.join(dataset_dir, ndarray_name)
 
     if not os.path.isfile(fpath):
-        response = requests.get(url, stream=True)
+        fetch_ndarray(url, fpath)
 
-        with open(fpath, "wb") as fin:
-            shutil.copyfileobj(response.raw, fin)
+    sha1 = sha1checksum(fpath)
+
+    assert sha1 == ndarray_hash, "Given hash does not match file hash"
 
     ndarray = numpy.load(fpath)
 
     return ndarray
+
+
+def load_split(
+    datasets_repository: str,
+    dataset_name: str,
+    split_id: str,
+    split_hashes: Tuple[str, str],
+    local_datasets_dir=str,
+):
+    x_name = "x_{}.ndy".format(split_id)
+    x_hash = split_hashes[0]
+
+    y_name = "y_{}.ndy".format(split_id)
+    y_hash = split_hashes[1]
+
+    x = load_ndarray(
+        datasets_repository=datasets_repository,
+        dataset_name=dataset_name,
+        ndarray_name=x_name,
+        ndarray_hash=x_hash,
+        local_datasets_dir=local_datasets_dir,
+    )
+
+    y = load_ndarray(
+        datasets_repository=datasets_repository,
+        dataset_name=dataset_name,
+        ndarray_name=y_name,
+        ndarray_hash=y_hash,
+        local_datasets_dir=local_datasets_dir,
+    )
+
+    return x, y
