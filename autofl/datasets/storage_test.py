b341c5837079f9a817218f4fd2dd65864a7c298a
Taner Topal <tanertopal42@gmail.com>
2019-07-05 11:10:03 +0200

    Remove wrong usage of xfail through better approach

--- autofl/datasets/storage_test.py
+++ autofl/datasets/storage_test.py
@@ -49,7 +49,6 @@ def test_load_ndarray(tmp_path):
     numpy.testing.assert_equal(ndarray_actual, ndarray_expected)
 
 
-@pytest.mark.xfail
 @pytest.mark.integration
 def test_load_ndarray_wrong_hash(tmp_path):
     # Prepare
@@ -58,9 +57,10 @@ def test_load_ndarray_wrong_hash(tmp_path):
     ndarray_hash = "wrong_hash"
 
     # Execute and expect to fail
-    storage.load_ndarray(
-        dataset_name=dataset_name,
-        ndarray_name=ndarray_name,
-        ndarray_hash=ndarray_hash,
-        local_datasets_dir=tmp_path,
-    )
+    with pytest.raises(Exception):
+        storage.load_ndarray(
+            dataset_name=dataset_name,
+            ndarray_name=ndarray_name,
+            ndarray_hash=ndarray_hash,
+            local_datasets_dir=tmp_path,
+        )
