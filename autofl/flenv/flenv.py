d7e617f2d33303493e57417740fd14e03a1ce94d
Daniel J. Beutel <daniel.beutel@xain.io>
2019-07-05 18:11:28 +0200

    Create first OpenAI Gym environment prototype

--- autofl/flenv/flenv.py
+++ autofl/flenv/flenv.py
@@ -1,8 +1,16 @@
-import random
 from typing import Any, Tuple
 
 import gym
+import numpy as np
+from absl import logging
 from gym.envs.registration import register
+
+from autofl.net import fc_compiled
+
+from ..datasets.api import fashion_mnist_10s_600_load_splits
+from ..fedml import Coordinator, Participant, RandomController
+
+NUM_ROUNDS = 7
 
 
 def register_gym_env():
@@ -10,18 +18,79 @@ def register_gym_env():
 
 
 class FederatedLearningEnv(gym.Env):
+
     metadata = {"render.modes": ["human"]}
 
     def __init__(self) -> None:
-        # TODO setup participants and coordinator
-        # TODO load data for each participant
-        print("FederatedLearningEvn initialized")
+        self.coordinator = init_fl()
+        # TODO override env.state_space and env.action_space
+        # FIXME probably wrong, suggests that action in {0, ..., NUM_PARTICIPANTS-1}
+        self.action_space = gym.spaces.Discrete(self.coordinator.num_participants())
+        self.observation_space = gym.spaces.Box(
+            low=0,
+            high=1,
+            shape=(NUM_ROUNDS, self.coordinator.num_participants()),
+            dtype=np.int8,
+        )
+        self.num_rounds = NUM_ROUNDS
+        self.state: np.ndarray = self._initial_state()
+        self.round = 0
+        self.prev_reward = 0.0
 
-    def step(self, action: str) -> Tuple[Any, float, bool, Any]:
-        return None, random.random(), False, None
+    def step(self, action: np.ndarray) -> Tuple[Any, float, bool, Any]:
+        # Ask coordinator to train using the provided participants
+        logging.info("FlEnv: step() received action {}".format(action))
+        indices = action_to_indices(action)
+        logging.info("FlEnv: Train using participants {}".format(indices))
+        self.coordinator.fit_round(indices)
+
+        # Estimate loss and accuracy
+        logging.info("FlEnv: Evaluate")
+        # TODO consider: full validation (or test?) set evaluation after last round
+        _, accuracy = self.coordinator.evaluate()
+        # Reward: Gain in validation set accuracy (estimated)
+        reward = accuracy - self.prev_reward
+        self.prev_reward = reward
+
+        # Update state: Override row of zeros with actual indices (i.e. the action taken)
+        # This results in a soft Markovian state which is basically an action log
+        self.state[self.round] = action
+
+        # Done: Terminate when limit is reached
+        self.round += 1
+        done = self.round == self.num_rounds
+        return np.copy(self.state), reward, done, None
 
     def reset(self) -> Any:
-        return None
+        self.state = self._initial_state()
+        self.round = 0
+        return np.copy(self.state)
 
     def render(self, mode="human") -> None:
         raise NotImplementedError()
+
+    def num_participants(self) -> int:
+        return self.coordinator.num_participants()
+
+    def _initial_state(self) -> np.ndarray:
+        return np.zeros((self.num_rounds, self.num_participants()))
+
+
+def action_to_indices(action: np.ndarray) -> np.ndarray:
+    return np.nonzero(action)[0]  # FIXME this is ugly
+
+
+def init_fl() -> Coordinator:
+    # xy_splits, xy_test = data.generate_splits_mnist(num_splits=10)
+    xy_splits, xy_test = fashion_mnist_10s_600_load_splits()
+    # Init participants
+    ps = []
+    for xy_split in xy_splits:
+        model = fc_compiled()
+        p = Participant(model, xy_split[0], xy_split[1])
+        ps.append(p)
+    # Init coordinator
+    # FIXME refactor: No controller needed
+    controller = RandomController(10, 3)
+    model = fc_compiled()
+    return Coordinator(controller, model, ps, xy_test)
--- autofl/flenv/flenv_test.py
+++ autofl/flenv/flenv_test.py
@@ -1,12 +1,27 @@
 import gym
+import numpy as np
 
 from autofl import flenv
 
+from .flenv import action_to_indices
 
-def test_FederatedLearningEnv_init():
+
+def test_action_to_indices():
+    # Prepare
+    action = np.array([0, 1, 0, 0, 1, 0])
+    expected = np.array([1, 4])
+    # Execute
+    actual = action_to_indices(action)
+    # Assert
+    np.testing.assert_equal(actual, expected)
+
+
+def test_FederatedLearningEnv_init_reset():
     # Prepare
     flenv.register_gym_env()
     # Execute
     env = gym.make("FederatedLearning-v0")
+    actual = env.reset()
     # Assert
     assert isinstance(env, flenv.FederatedLearningEnv)
+    assert actual.shape[1] == env.num_participants()
