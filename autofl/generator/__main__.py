5529cd7f43240085d43bef496e3b1b970a8e3e17
Taner Topal <tanertopal42@gmail.com>
2019-07-05 11:08:05 +0200

    Fixes linter error

--- autofl/generator/__main__.py
+++ autofl/generator/__main__.py
@@ -1,5 +1,5 @@
 from .datasets import generate_cifar10_random_splits_10, generate_fashion_mnist_10s_600
 
 # Just uncomment the methods you want to run
-# generate_cifar10_random_splits_10()
+generate_cifar10_random_splits_10()
 generate_fashion_mnist_10s_600()
