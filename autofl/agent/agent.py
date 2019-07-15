27bd54719f2c69fe0f4b104fad9cfa7cc7fd7dd5
Daniel J. Beutel <daniel.beutel@xain.io>
2019-07-15 08:08:43 +0200

    Use FederatedLearning-v0 environment in autofl.agent

--- autofl/agent/agent.py
+++ autofl/agent/agent.py
@@ -1,5 +1,13 @@
+import gym
 from absl import logging
+
+from ..flenv import register_gym_env
 
 
 def main(_):
-    logging.info("Starting to train FedML Agent")
+    logging.set_verbosity(logging.DEBUG)
+
+    register_gym_env()
+    env = gym.make("FederatedLearning-v0")
+    print("action_space:      {}".format(env.action_space))
+    print("observation_space: {}".format(env.observation_space))
