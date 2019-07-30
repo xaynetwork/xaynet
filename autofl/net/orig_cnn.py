59540c856a360dab655552fc10247567ab86fb79
Daniel J. Beutel <daniel.beutel@xain.io>
2019-07-30 10:00:07 +0200

    Make CNN initialization reproducible

--- autofl/net/orig_cnn.py
+++ autofl/net/orig_cnn.py
@@ -1,16 +1,24 @@
+from typing import Optional
+
 import tensorflow as tf
 from tensorflow.keras.layers import Conv2D, Dense, Flatten, Input, MaxPool2D
 
 
-def orig_cnn_compiled(input_shape=(28, 28, 1), num_classes=10) -> tf.keras.Model:
+def orig_cnn_compiled(
+    input_shape=(28, 28, 1), num_classes=10, seed: Optional[int] = None
+) -> tf.keras.Model:
+    # Kernel initializer
+    ki = tf.keras.initializers.glorot_uniform(seed=seed)
+
+    # Architecture
     inputs = Input(shape=input_shape)
-    x = Conv2D(32, kernel_size=5, activation="relu")(inputs)
+    x = Conv2D(32, kernel_size=5, kernel_initializer=ki, activation="relu")(inputs)
     x = MaxPool2D(pool_size=2)(x)
-    x = Conv2D(64, kernel_size=5, activation="relu")(x)
+    x = Conv2D(64, kernel_size=5, kernel_initializer=ki, activation="relu")(x)
     x = MaxPool2D(pool_size=2)(x)
     x = Flatten()(x)
-    x = Dense(512, activation="relu")(x)
-    outputs = Dense(num_classes, activation="softmax")(x)
+    x = Dense(512, kernel_initializer=ki, activation="relu")(x)
+    outputs = Dense(num_classes, kernel_initializer=ki, activation="softmax")(x)
 
     model = tf.keras.Model(inputs=inputs, outputs=outputs)
 
