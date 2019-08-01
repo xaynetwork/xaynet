c1045560809447e2873a5ea453258b9cc432ec9f
Taner Topal <tanertopal42@gmail.com>
2019-08-01 14:28:14 +0200

    Add docker push to script

--- scripts/docker_build_image.sh
+++ scripts/docker_build_image.sh
@@ -3,12 +3,26 @@
 # Exit immediately if a command exits with a non-zero status.
 set -e
 
+# Do this early to avoid building if
+# credentials are not even setup
+export AWS_PROFILE=xain-autofl
+$(aws ecr get-login --no-include-email)
+
+ECR_REPO="693828385217.dkr.ecr.eu-central-1.amazonaws.com/autofl"
 DOCKERFILE="ops/docker/Dockerfile"
-# IMAGE_TAG=$(date +%s)
-IMAGE_TAG="autofl"
+IMAGE_NAME="autofl"
+IMAGE_TAG=$(python -c "import time; print(int(time.time() / 60))")
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 cd $DIR/../
 
-docker build -f $DOCKERFILE -t $IMAGE_TAG .
+
+docker build \
+    -f $DOCKERFILE \
+    -t $IMAGE_NAME:latest \
+    -t $IMAGE_NAME:$IMAGE_TAG \
+    -t $ECR_REPO:$IMAGE_TAG \
+    .
+
+docker push $ECR_REPO
