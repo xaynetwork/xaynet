a1a04b334ff8250f3c30d7682b12f6d0a816f5fd
Taner Topal <tanertopal42@gmail.com>
2019-08-05 12:50:07 +0200

    Upgrade pip to silence a warning

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -7,6 +7,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 cd $DIR/../
 
-pip install -U pip==19.1.1
+pip install -U pip==19.2.1
 pip install -U setuptools==41.0.1
 pip install -e .[dev,cpu]
