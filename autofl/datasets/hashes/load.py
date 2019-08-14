72fecb5224cf8ab1e4de7d5f9c8062b2aab47045
Taner Topal <tanertopal42@gmail.com>
2019-08-14 14:26:02 +0200

    Move hashes into extra directory in preparation of a bigger refactoring

---
+++ autofl/datasets/hashes/load.py
@@ -0,0 +1,16 @@
+import glob
+import json
+import os
+
+
+def load():
+    datasets = {}
+    path = os.path.dirname(__file__)
+
+    json_pattern = os.path.join(path, "*.json")
+    file_list = glob.glob(json_pattern)
+
+    for fname in file_list:
+        with open(fname) as f:
+            dict_key = os.path.basename(fname)[0:-5]
+            datasets[dict_key] = json.loads(f.read())
