437fd8e833be52330517a98f9c6d025d2db3910b
Taner Topal <tanertopal42@gmail.com>
2019-08-16 13:42:53 +0200

    Make benchmarks selectable through cmd option
    To enable triggering multiple benchmarks with the same docker image through
    a shell script we will select the benchmark to run with a cmd option.
    
    In the future starting a benchmark can be done as follows:
    train --benchmark_name fashion_mnist_100p_09cpp

--- autofl/benchmark/__main__.py
+++ autofl/benchmark/__main__.py
@@ -1,6 +1,19 @@
-from absl import app
+from absl import app, flags
 
 from autofl.benchmark import bench_fl
+
+# following: https://abseil.io/docs/cpp/guides/flags#flags-best-practices
+# we will define our flags in this file
+FLAGS = flags.FLAGS
+
+flags.DEFINE_string(
+    "benchmark_name",
+    None,
+    "Name of the benchmark to be run. Example: bench_fl.py"
+    + " - other modules should use it in similar ways",
+)
+
+flags.mark_flag_as_required("benchmark_name")
 
 
 def main():
