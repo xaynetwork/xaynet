d4988a9d75c4c2e25e5a5fc23f96169d583f0e01
Daniel J. Beutel <daniel.beutel@xain.io>
2019-08-19 10:18:58 +0200

    Create new model blog_cnn

--- autofl/net/__init__.py
+++ autofl/net/__init__.py
@@ -1,3 +1,4 @@
+from .blog_cnn import blog_cnn_compiled
 from .orig_2nn import orig_2nn_compiled
 from .orig_cnn import orig_cnn_compiled
 from .resnet import resnet20v2_compiled
---
+++ autofl/net/blog_cnn.py
@@ -0,0 +1,46 @@
+import tensorflow as tf
+from tensorflow.keras.layers import Conv2D, Dense, Dropout, Flatten, MaxPool2D
+
+# Derived from: https://medium.com/tensorflow/hello-deep-learning-fashion-mnist-with-keras-50fcff8cd74a
+
+
+def blog_cnn_compiled(
+    input_shape=(28, 28, 1), num_classes: int = 10, seed: int = 1096
+) -> tf.keras.Model:
+    ki = tf.keras.initializers.glorot_uniform(seed=seed)
+
+    model = tf.keras.Sequential()
+    # Must define the input shape in the first layer of the neural network
+    model.add(
+        Conv2D(
+            filters=64,
+            kernel_size=2,
+            padding="same",
+            activation="relu",
+            kernel_initializer=ki,
+            input_shape=input_shape,
+        )
+    )
+    model.add(MaxPool2D(pool_size=2))
+    model.add(Dropout(0.3))
+    model.add(
+        Conv2D(
+            filters=32,
+            kernel_size=2,
+            padding="same",
+            activation="relu",
+            kernel_initializer=ki,
+        )
+    )
+    model.add(MaxPool2D(pool_size=2))
+    model.add(Dropout(0.3))
+    model.add(Flatten())
+    model.add(Dense(256, activation="relu", kernel_initializer=ki))
+    model.add(Dropout(0.5))
+    model.add(Dense(num_classes, activation="softmax", kernel_initializer=ki))
+
+    # Compile model
+    model.compile(
+        loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"]
+    )
+    return model
