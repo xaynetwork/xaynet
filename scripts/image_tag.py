9fa401fadf6dfe44a22dc12ff52012cd8fec745a
Taner Topal <tanertopal42@gmail.com>
2019-08-21 13:29:06 +0200

    Remove seconds from IMAGE_TAG and move optional benchmark group after timestamp

--- scripts/image_tag.py
+++ scripts/image_tag.py
@@ -7,12 +7,12 @@ from faker.providers import person
 fake = Faker()
 fake.add_provider(person)
 
-utc_time = strftime("%Y%m%dT%H%M%S")
+utc_time = strftime("%Y%m%dT%H%M")
 fake_name = fake.name().replace(" ", "_").lower()
 
 if os.environ.get("BENCHMARK_GROUP"):
-    benchmark_group = f"{os.environ['BENCHMARK_GROUP']}_"
+    benchmark_group = f"_{os.environ['BENCHMARK_GROUP']}_"
 else:
-    benchmark_group = ""
+    benchmark_group = "_"
 
-print(f"{benchmark_group}{utc_time}_{fake_name}")
+print(f"{utc_time}{benchmark_group}{fake_name}")
