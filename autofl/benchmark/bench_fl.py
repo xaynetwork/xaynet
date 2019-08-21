004e859595092b739bd772f74b71283a7104750d
Taner Topal <tanertopal42@gmail.com>
2019-08-21 15:07:53 +0200

    Enable running bench_fl and bench_ea directly and fixes issue when starting train console_script

--- autofl/benchmark/bench_fl.py
+++ autofl/benchmark/bench_fl.py
@@ -1,7 +1,7 @@
 import time
 from typing import List, Optional, Tuple
 
-from absl import flags, logging
+from absl import app, flags, logging
 
 from autofl.datasets import load_splits
 
@@ -115,7 +115,12 @@ def run_unitary_versus_federated(
     report.plot_accuracies(plot_data, fname="plot.png")
 
 
-def main():
+def main(_):
     benchmark_name = FLAGS.benchmark_name
     kwargs = benchmarks[benchmark_name]
     run_unitary_versus_federated(benchmark_name=benchmark_name, **kwargs)
+
+
+if __name__ == "__main__":
+    FLAGS(["_", "--benchmark_name=fashion_mnist_100p_IID_balanced"])
+    app.run(main=main)
