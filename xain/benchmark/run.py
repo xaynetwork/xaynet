ab6a135160b81937871bb114644241126045e879
Daniel J. Beutel <daniel.beutel@xain.io>
2019-08-29 12:54:27 +0200

    Refactor Fashion-MNIST benchmarking in preparation for CIFAR benchmarks

--- xain/benchmark/run.py
+++ xain/benchmark/run.py
@@ -1,18 +1,31 @@
 import random
-from typing import List, Tuple
+import time
+from typing import List, Optional, Tuple
 
 import numpy as np
 import tensorflow as tf
+from absl import logging
 
 from xain.benchmark.net import orig_cnn_compiled
+from xain.datasets import load_splits
 from xain.fl.coordinator import Coordinator, RandomController
 from xain.fl.coordinator.aggregate import Aggregator
 from xain.fl.participant import ModelProvider, Participant
+from xain.helpers import storage
 from xain.types import FederatedDatasetPartition, KerasHistory
+
+from . import report
 
 random.seed(0)
 np.random.seed(1)
 tf.compat.v1.set_random_seed(2)
+
+
+# Default parameters for `unitary_versus_federated`
+FLH_C = 0.1  # Fraction of participants used in each round of training
+ROUNDS = 50  # Number of total rounds to train
+FLH_E = 4  # Number of training epochs in each round
+FLH_B = 64  # Batch size used by participants
 
 
 # pylint: disable-msg=too-many-locals
@@ -97,3 +110,77 @@ def federated_training(
 
     # Report results
     return hist_co, hist_ps, loss, acc
+
+
+def unitary_versus_federated(
+    benchmark_name: str,
+    dataset_name: str,
+    C: float = FLH_C,
+    E: int = FLH_E,
+    B: int = FLH_B,
+    rounds: int = ROUNDS,
+):
+    """
+    :param C: Fraction of participants used in each round of training
+    """
+    logging.info(f"Starting {benchmark_name}")
+    xy_train_partitions, xy_val, xy_test = load_splits(dataset_name)
+
+    start = time.time()
+
+    # Train CNN on a single partition ("unitary learning")
+    # TODO train n models on all partitions
+    partition_id = 0
+    xy_train = xy_train_partitions[partition_id]
+    logging.info(f"Run unitary training using partition {partition_id}")
+    ul_hist, ul_loss, ul_acc = unitary_training(
+        xy_train, xy_val, xy_test, epochs=rounds * E, batch_size=B
+    )
+
+    # Train CNN using federated learning on all partitions
+    logging.info("Run federated learning using all partitions")
+    fl_hist, _, fl_loss, fl_acc = federated_training(
+        xy_train_partitions, xy_val, xy_test, rounds, C=C, E=E, B=B
+    )
+
+    end = time.time()
+
+    # Write results JSON
+    results = {
+        "name": benchmark_name,
+        "start": start,
+        "end": end,
+        "duration": end - start,
+        "FLH_C": C,
+        "FLH_E": E,
+        "FLH_B": B,
+        "ROUNDS": rounds,
+        "unitary_learning": {
+            "loss": float(ul_loss),
+            "acc": float(ul_acc),
+            "hist": ul_hist,
+        },
+        "federated_learning": {
+            "loss": float(fl_loss),
+            "acc": float(fl_acc),
+            "hist": fl_hist,
+        },
+    }
+    storage.write_json(results, fname="results.json")
+
+    # Plot results
+    # TODO include aggregated participant histories in plot
+    plot_data: List[Tuple[str, List[float], Optional[List[int]]]] = [
+        (
+            "Unitary Learning",
+            ul_hist["val_acc"],
+            [i for i in range(1, len(ul_hist["val_acc"]) + 1, 1)],
+        ),
+        (
+            "Federated Learning",
+            fl_hist["val_acc"],
+            [i for i in range(E, len(fl_hist["val_acc"]) * E + 1, E)],
+        ),
+    ]
+    # FIXME use different filenames for different datasets
+    report.plot_accuracies(plot_data, fname="plot.png")
