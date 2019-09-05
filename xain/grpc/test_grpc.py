3f7a1f9903818ce0c297fb7ee4a34cb73fdf1fc8
Rodolphe Marques <marques.rodolphe@gmail.com>
2019-09-05 14:45:20 +0200

    Fix formatting

--- xain/grpc/test_grpc.py
+++ xain/grpc/test_grpc.py
@@ -1,9 +1,9 @@
 from concurrent import futures
 
-import pytest
 import grpc
+import pytest
 
-from xain.grpc import helloworld_pb2_grpc, helloworld_pb2
+from xain.grpc import helloworld_pb2, helloworld_pb2_grpc
 from xain.grpc.greeter_server import Greeter
 
 
