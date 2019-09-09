fa7ff935b1e4815df6dc61e5f00eb3d78f3ac652
Taner Topal <tanertopal42@gmail.com>
2019-09-09 14:29:14 +0200

    Add file based global config setup
    This change will automatically init a global config file in project root where the user can centrally configure the project
    Some config flags will serve as defaults for various abseil flags

--- xain/benchmark/__init__.py
+++ xain/benchmark/__init__.py
@@ -1,10 +1,14 @@
 from absl import flags
 
+import xain.config
+
 from .benchmark import benchmarks
+
+config = xain.config.load()
 
 flags.DEFINE_string(
     "benchmark_name",
-    "flul-fashion-mnist-100p-iid-balanced",
+    config.get("Benchmark", "default_benchmark_name", fallback=None),
     f"One of: {[k for k in benchmarks]}",
 )
 
