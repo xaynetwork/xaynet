441a8bab26ee7a8790f59dc4bc6fd701656bc074
Rodolphe Marques <marques.rodolphe@gmail.com>
2019-09-13 17:51:24 +0200

    Added tests for the grpc rendezvous method

--- xain/grpc/test_grpc.py
+++ xain/grpc/test_grpc.py
@@ -1,24 +1,20 @@
 from concurrent import futures
+from unittest import mock
 
 import grpc
 import numpy as np
 import pytest
+from google.protobuf import empty_pb2
 from numproto import ndarray_to_proto, proto_to_ndarray
 
-from xain.grpc import hellonumproto_pb2, hellonumproto_pb2_grpc
+from xain.grpc import (
+    coordinator_pb2,
+    coordinator_pb2_grpc,
+    hellonumproto_pb2,
+    hellonumproto_pb2_grpc,
+)
+from xain.grpc.coordinator import Coordinator
 from xain.grpc.numproto_server import NumProtoServer
-
-
-@pytest.fixture
-def greeter_server():
-    server = grpc.server(futures.ThreadPoolExecutor(max_workers=1))
-    hellonumproto_pb2_grpc.add_NumProtoServerServicer_to_server(
-        NumProtoServer(), server
-    )
-    server.add_insecure_port("localhost:50051")
-    server.start()
-    yield
-    server.stop(0)
 
 
 # pylint: disable=W0613,W0621
@@ -35,3 +31,35 @@ def test_greeter_server(greeter_server):
         response_nda = proto_to_ndarray(response.arr)
 
         assert np.array_equal(nda * 2, response_nda)
+
+
+@pytest.mark.integration
+def test_participant_rendezvous_accept(participant_stub, coordinator_service):
+    reply = participant_stub.Rendezvous(empty_pb2.Empty())
+
+    assert reply.response == coordinator_pb2.RendezvousResponse.ACCEPT
+
+
+def mocked_init(self, required_participants=10):
+    """Sets `n_participants` to be the same as `required_participants` so that
+    the coordinator tells the client to try later.
+    """
+    self.required_participants = 10
+    self.n_participants = 10
+
+
+@pytest.mark.integration
+@mock.patch("xain.grpc.coordinator.Coordinator.__init__", new=mocked_init)
+def test_participant_rendezvous_later(participant_stub):
+
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=1))
+    coordinator_pb2_grpc.add_CoordinatorServicer_to_server(Coordinator(), server)
+    server.add_insecure_port("localhost:50051")
+
+    server.start()
+
+    reply = participant_stub.Rendezvous(empty_pb2.Empty())
+
+    server.stop(0)
+
+    assert reply.response == coordinator_pb2.RendezvousResponse.LATER
