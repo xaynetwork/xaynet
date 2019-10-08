af674bdb808f8b9b157ddb6b28695123b3518f64
finiteprods <kwok.doc@gmail.com>
2019-10-08 05:49:47 +0100

    coordinator and participant generic in training data

--- xain/grpc/participant.py
+++ xain/grpc/participant.py
@@ -1,11 +1,13 @@
 import threading
 import time
+from typing import Tuple
 
 import grpc
 import numpy as np
 from numproto import ndarray_to_proto, proto_to_ndarray
 
 from xain.grpc import coordinator_pb2, coordinator_pb2_grpc
+from xain.types import History, Metrics, Theta
 
 RETRY_TIMEOUT = 5
 HEARTBEAT_TIME = 10
@@ -35,47 +37,67 @@ def rendezvous(channel):
         response = reply.response
 
 
-def start_training(channel):
+def start_training(channel) -> Tuple[Theta, int, int]:
     stub = coordinator_pb2_grpc.CoordinatorStub(channel)
     req = coordinator_pb2.StartTrainingRequest()
     # send request to start training
     reply = stub.StartTraining(req)
     print(f"Participant received: {type(reply)}")
     theta, epochs, epoch_base = reply.theta, reply.epochs, reply.epoch_base
-    print(f"epochs: {epochs}, epoch_base: {epoch_base}")
+    # print(f"epochs: {epochs}, epoch_base: {epoch_base}")
     # iterate over theta, deserialising and logging...
-    print(f"theta of type {type(theta)}:")
-    for proto_nda in theta:
-        nda = proto_to_ndarray(proto_nda)
-        print(f"{nda}")
+    # print(f"theta of type {type(theta)}:")
+    # for proto_nda in theta:
+    #     nda = proto_to_ndarray(proto_nda)
+    #     print(f"{nda}")
     # now simulate some training...
+    return [proto_to_ndarray(pnda) for pnda in theta], epochs, epoch_base
 
 
-def end_training(channel):
+def end_training(
+    channel,
+    theta_update: Tuple[Theta, int], history: History, metrics: Metrics
+):
     stub = coordinator_pb2_grpc.CoordinatorStub(channel)
     # build request starting with theta update
-    nda1, nda2 = np.arange(20, 30), np.arange(30, 40)
-    num = 2
-    print(f"Participant requests: theta' {nda1}, {nda2}; {num} examples")
-    theta_prime = [ndarray_to_proto(nda1), ndarray_to_proto(nda2)]
+
+    # nda1, nda2 = np.arange(20, 30), np.arange(30, 40)
+    # num = 2
+    # print(f"Participant requests: theta' {nda1}, {nda2}; {num} examples")
+    # theta_prime = [ndarray_to_proto(nda1), ndarray_to_proto(nda2)]
+    # theta_update = coordinator_pb2.EndTrainingRequest.ThetaUpdate(
+    #     theta_prime=theta_prime, num_examples=num)
+    theta, num = theta_update
     theta_update = coordinator_pb2.EndTrainingRequest.ThetaUpdate(
-        theta_prime=theta_prime, num_examples=num
+        theta_prime=[ndarray_to_proto(nda) for nda in theta],
+        num_examples=num
     )
     # history data
-    k1, k2 = "aaa", "bbb"
-    v1, v2 = [1.1, 2.1], [3.1, 4.1]
-    print(f"History data: {k1}: {v1}; {k2}: {v2}")
-    his = {
-        k1: coordinator_pb2.EndTrainingRequest.HistoryValue(values=v1),
-        k2: coordinator_pb2.EndTrainingRequest.HistoryValue(values=v2),
-    }
+
+    # k1, k2 = "aaa", "bbb"
+    # v1, v2 = [1.1, 2.1], [3.1, 4.1]
+    # print(f"History data: {k1}: {v1}; {k2}: {v2}")
+    # his = {
+    #     k1: coordinator_pb2.EndTrainingRequest.HistoryValue(values=v1),
+    #     k2: coordinator_pb2.EndTrainingRequest.HistoryValue(values=v2),
+    # }
+    h = {k: coordinator_pb2.EndTrainingRequest.HistoryValue(values=v)
+         for k, v in history.items()}
     # metrics
-    cid, vbc = 1, [3, 4, 5]
-    print(f"Metrics: cid {cid}, vol by class {vbc}")
-    met = coordinator_pb2.EndTrainingRequest.Metrics(cid=cid, vol_by_class=vbc)
+
+    # cid, vbc = 1, [3, 4, 5]
+    # print(f"Metrics: cid {cid}, vol by class {vbc}")
+    # met = coordinator_pb2.EndTrainingRequest.Metrics(cid=cid, vol_by_class=vbc)
+    cid, vbc = metrics
+    m = coordinator_pb2.EndTrainingRequest.Metrics(cid=cid, vol_by_class=vbc)
+
     # assemble req
+
+    # req = coordinator_pb2.EndTrainingRequest(
+    #     theta_update=theta_update, history=his, metrics=met
+    # )
     req = coordinator_pb2.EndTrainingRequest(
-        theta_update=theta_update, history=his, metrics=met
+        theta_update=theta_update, history=h, metrics=m
     )
     # send request to end training
     reply = stub.EndTraining(req)
@@ -90,22 +112,24 @@ def run():
     rendezvous(channel)
 
     # start the heartbeat in a different thread
-    terminate_event = threading.Event()
-    heartbeat_thread = threading.Thread(
-        target=heartbeat, args=(channel, terminate_event)
-    )
-    heartbeat_thread.start()
+    # terminate_event = threading.Event()
+    # heartbeat_thread = threading.Thread(
+    #     target=heartbeat, args=(channel, terminate_event)
+    # )
+    # heartbeat_thread.start()
 
-    # do a training round
-    start_training(channel)
-    end_training(channel)
+    # sample training round
+    theta, epochs, epoch_base = start_training(channel)
+    # end training with some default values; in actual training you'd pass
+    # along the theta_update, history and metrics to end the round
+    end_training(channel, (theta, 0), {}, (0, []))
 
-    try:
-        # never returns unless there is an exception
-        heartbeat_thread.join()
-    except KeyboardInterrupt:
-        terminate_event.set()
-        channel.close()
+    # try:
+    #     # never returns unless there is an exception
+    #     heartbeat_thread.join()
+    # except KeyboardInterrupt:
+    #     terminate_event.set()
+    #     channel.close()
 
 
 if __name__ == "__main__":
