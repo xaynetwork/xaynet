2deb7cf3347f14028bc5e5130d35318e9eeee681
Daniel J. Beutel <daniel.beutel@xain.io>
2019-10-16 18:37:24 +0100

    Print weight dimensions of 2NN/CNN/ResNet

--- xain/benchmark/net/__main__.py
+++ xain/benchmark/net/__main__.py
@@ -6,21 +6,35 @@ from .resnet import resnet20v2_compiled
 
 
 def main(_):
+    print_model_summaries()
+
+
+def print_model_summaries():
     logging.info("\nOrig. 2NN:")
     model = orig_2nn_compiled()
     model.summary(print_fn=logging.info)
+    print_weight_dims(model)
 
     logging.info("\nOrig. CNN (MNIST):")
     model = orig_cnn_compiled()
     model.summary(print_fn=logging.info)
+    print_weight_dims(model)
 
     logging.info("\nOrig. CNN (CIFAR-10):")
     model = orig_cnn_compiled(input_shape=(32, 32, 3))
     model.summary(print_fn=logging.info)
+    print_weight_dims(model)
 
     logging.info("\nResNet20v2 (CIFAR-10):")
     model = resnet20v2_compiled(input_shape=(32, 32, 3))
     model.summary(print_fn=logging.info)
+    print_weight_dims(model)
+
+
+def print_weight_dims(model):
+    weights = model.get_weights()
+    for w in weights:
+        print(w.shape)
 
 
 if __name__ == "__main__":
