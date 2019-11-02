95635ff4eec5cc3ada49ae51d8f151810c295601
Daniel J. Beutel <daniel.beutel@xain.io>
2019-11-02 17:09:37 +0100

    Add Python 3.8 classifier to setup.py

--- setup.py
+++ setup.py
@@ -128,6 +128,7 @@ setup(
         "Programming Language :: Python :: 3 :: Only",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Operating System :: MacOS :: MacOS X",
         "Operating System :: POSIX :: Linux",
     ],
