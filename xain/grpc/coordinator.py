06da807833a748d6f1b0255a07fdc901b4fc9fdc
Rodolphe Marques <marques.rodolphe@gmail.com>
2019-11-07 16:16:45 +0100

    Renamed references of peer with participant
    - More consistent terminology

--- xain/grpc/coordinator.py
+++ xain/grpc/coordinator.py
@@ -160,7 +160,7 @@ class Round:
         """Valid a participant's update for the round.
 
         Args:
-            participant_id (:obj:`str`): The id of the peer making the request.
+            participant_id (:obj:`str`): The id of the participant making the request.
             theta_update (:obj:`tuple` of :obj:`list` of :class:`~numpy.ndarray`):
                 A tuple containing a list of updated weights.
             history (:obj:`dict`): TODO
@@ -280,32 +280,32 @@ class Coordinator:
         self.current_round = 0
 
     def on_message(
-        self, message: GeneratedProtocolMessageType, peer_id: str
+        self, message: GeneratedProtocolMessageType, participant_id: str
     ) -> GeneratedProtocolMessageType:
         """Coordinator method that implements the state machine.
 
         Args:
             message (:class:`~.GeneratedProtocolMessageType`): A protobuf message.
-            peer_id (:obj:`str`): The id of the peer making the request.
+            participant_id (:obj:`str`): The id of the participant making the request.
 
         Returns:
-            :class:`~.GeneratedProtocolMessageType`: The reply to be sent back to the peer.
+            :class:`~.GeneratedProtocolMessageType`: The reply to be sent back to the participant.
 
         Raises:
             :class:`~UnknownParticipantError`: If it receives a request from an
             unknown participant. Typically a participant that has not
             rendezvous with the :class:`~.Coordinator`.
         """
-        logger.debug("Received: %s from %s", type(message), peer_id)
+        logger.debug("Received: %s from %s", type(message), participant_id)
 
         # Unless this is a RendezvousRequest the coordinator should not accept messages
         # from participants that have not been accepted
         if (
             not isinstance(message, coordinator_pb2.RendezvousRequest)
-            and peer_id not in self.participants.participants.keys()
+            and participant_id not in self.participants.participants.keys()
         ):
             raise UnknownParticipantError(
-                f"Unknown participant {peer_id}. "
+                f"Unknown participant {participant_id}. "
                 "Please try to rendezvous with the coordinator before making a request."
             )
 
@@ -323,9 +323,11 @@ class Coordinator:
 
             if self.participants.len() < self.required_participants:
                 response = coordinator_pb2.RendezvousResponse.ACCEPT
-                self.participants.add(peer_id)
+                self.participants.add(participant_id)
                 logger.info(
-                    "Accepted %s. Participants: %d", peer_id, self.participants.len()
+                    "Accepted %s. Participants: %d",
+                    participant_id,
+                    self.participants.len(),
                 )
 
                 # Change the state to ROUND if we are in STANDBY and already
@@ -340,7 +342,7 @@ class Coordinator:
                 response = coordinator_pb2.RendezvousResponse.LATER
                 logger.info(
                     "Reject participant %s. Participants: %d",
-                    peer_id,
+                    participant_id,
                     self.participants.len(),
                 )
 
@@ -349,7 +351,7 @@ class Coordinator:
         elif isinstance(message, coordinator_pb2.HeartbeatRequest):
             # Handle heartbeat
 
-            self.participants.update_expires(peer_id)
+            self.participants.update_expires(participant_id)
 
             # send heartbeat reply advertising the current state
             return coordinator_pb2.HeartbeatReply(
@@ -378,7 +380,7 @@ class Coordinator:
             theta_update = [proto_to_ndarray(pnda) for pnda in tp], num
             history = {k: list(hv.values) for k, hv in his.items()}
             metrics = (cid, list(vbc))
-            self.round.add_updates(peer_id, theta_update, history, metrics)
+            self.round.add_updates(participant_id, theta_update, history, metrics)
 
             # The round is over. Run the aggregation
             if self.round.is_finished():
--- xain/grpc/test_coordinator_logic.py
+++ xain/grpc/test_coordinator_logic.py
@@ -12,7 +12,7 @@ from xain.grpc.coordinator import (
 
 def test_rendezvous_accept():
     coordinator = Coordinator()
-    result = coordinator.on_message(coordinator_pb2.RendezvousRequest(), "peer1")
+    result = coordinator.on_message(coordinator_pb2.RendezvousRequest(), "participant1")
 
     assert isinstance(result, coordinator_pb2.RendezvousReply)
     assert result.response == coordinator_pb2.RendezvousResponse.ACCEPT
@@ -20,8 +20,8 @@ def test_rendezvous_accept():
 
 def test_rendezvous_later():
     coordinator = Coordinator(required_participants=1)
-    coordinator.on_message(coordinator_pb2.RendezvousRequest(), "peer1")
-    result = coordinator.on_message(coordinator_pb2.RendezvousRequest(), "peer2")
+    coordinator.on_message(coordinator_pb2.RendezvousRequest(), "participant1")
+    result = coordinator.on_message(coordinator_pb2.RendezvousRequest(), "participant2")
 
     assert isinstance(result, coordinator_pb2.RendezvousReply)
     assert result.response == coordinator_pb2.RendezvousResponse.LATER
@@ -30,8 +30,8 @@ def test_rendezvous_later():
 def test_heartbeat_reply():
     # test that the coordinator replies with the correct state and round number
     coordinator = Coordinator()
-    coordinator.on_message(coordinator_pb2.RendezvousRequest(), "peer1")
-    result = coordinator.on_message(coordinator_pb2.HeartbeatRequest(), "peer1")
+    coordinator.on_message(coordinator_pb2.RendezvousRequest(), "participant1")
+    result = coordinator.on_message(coordinator_pb2.HeartbeatRequest(), "participant1")
 
     assert isinstance(result, coordinator_pb2.HeartbeatReply)
     assert result.state == coordinator_pb2.State.STANDBY
@@ -40,7 +40,7 @@ def test_heartbeat_reply():
     # update the round and state of the coordinator and check again
     coordinator.state = coordinator_pb2.State.ROUND
     coordinator.current_round = 10
-    result = coordinator.on_message(coordinator_pb2.HeartbeatRequest(), "peer1")
+    result = coordinator.on_message(coordinator_pb2.HeartbeatRequest(), "participant1")
 
     assert result.state == coordinator_pb2.State.ROUND
     assert result.round == 10
@@ -53,7 +53,7 @@ def test_state_standby_round():
 
     assert coordinator.state == coordinator_pb2.STANDBY
 
-    coordinator.on_message(coordinator_pb2.RendezvousRequest(), "peer1")
+    coordinator.on_message(coordinator_pb2.RendezvousRequest(), "participant1")
 
     assert coordinator.state == coordinator_pb2.State.ROUND
     assert coordinator.current_round == 1
@@ -62,9 +62,11 @@ def test_state_standby_round():
 def test_start_training():
     test_theta = [np.arange(10), np.arange(10, 20)]
     coordinator = Coordinator(required_participants=1, theta=test_theta)
-    coordinator.on_message(coordinator_pb2.RendezvousRequest(), "peer1")
+    coordinator.on_message(coordinator_pb2.RendezvousRequest(), "participant1")
 
-    result = coordinator.on_message(coordinator_pb2.StartTrainingRequest(), "peer1")
+    result = coordinator.on_message(
+        coordinator_pb2.StartTrainingRequest(), "participant1"
+    )
     received_theta = [proto_to_ndarray(nda) for nda in result.theta]
 
     np.testing.assert_equal(test_theta, received_theta)
@@ -75,27 +77,27 @@ def test_end_training():
     # with only one participant it wouldn't work because the local updates state is cleaned at
     # the end of each round
     coordinator = Coordinator(required_participants=2)
-    coordinator.on_message(coordinator_pb2.RendezvousRequest(), "peer1")
-    coordinator.on_message(coordinator_pb2.RendezvousRequest(), "peer2")
+    coordinator.on_message(coordinator_pb2.RendezvousRequest(), "participant1")
+    coordinator.on_message(coordinator_pb2.RendezvousRequest(), "participant2")
 
-    coordinator.on_message(coordinator_pb2.EndTrainingRequest(), "peer1")
+    coordinator.on_message(coordinator_pb2.EndTrainingRequest(), "participant1")
 
     assert len(coordinator.round.updates) == 1
 
 
 def test_end_training_round_update():
-    # Test that the round number is updated once all participants send their updates
+    # Test that the round number is updated once all participants sent their updates
     coordinator = Coordinator(required_participants=2)
-    coordinator.on_message(coordinator_pb2.RendezvousRequest(), "peer1")
-    coordinator.on_message(coordinator_pb2.RendezvousRequest(), "peer2")
+    coordinator.on_message(coordinator_pb2.RendezvousRequest(), "participant1")
+    coordinator.on_message(coordinator_pb2.RendezvousRequest(), "participant2")
 
     # check that we are currently in round 1
     assert coordinator.current_round == 1
 
-    coordinator.on_message(coordinator_pb2.EndTrainingRequest(), "peer1")
+    coordinator.on_message(coordinator_pb2.EndTrainingRequest(), "participant1")
     # check we are still in round 1
     assert coordinator.current_round == 1
-    coordinator.on_message(coordinator_pb2.EndTrainingRequest(), "peer2")
+    coordinator.on_message(coordinator_pb2.EndTrainingRequest(), "participant2")
 
     # check that round number was updated
     assert coordinator.current_round == 2
@@ -103,15 +105,15 @@ def test_end_training_round_update():
 
 def test_end_training_reinitialize_local_models():
     coordinator = Coordinator(required_participants=2)
-    coordinator.on_message(coordinator_pb2.RendezvousRequest(), "peer1")
-    coordinator.on_message(coordinator_pb2.RendezvousRequest(), "peer2")
+    coordinator.on_message(coordinator_pb2.RendezvousRequest(), "participant1")
+    coordinator.on_message(coordinator_pb2.RendezvousRequest(), "participant2")
 
-    coordinator.on_message(coordinator_pb2.EndTrainingRequest(), "peer1")
+    coordinator.on_message(coordinator_pb2.EndTrainingRequest(), "participant1")
 
     # After one participant sends its updates we should have one update in the coordinator
     assert len(coordinator.round.updates) == 1
 
-    coordinator.on_message(coordinator_pb2.EndTrainingRequest(), "peer2")
+    coordinator.on_message(coordinator_pb2.EndTrainingRequest(), "participant2")
 
     # once the second participant delivers its updates the round ends and the local models
     # are reinitialized
@@ -120,11 +122,11 @@ def test_end_training_reinitialize_local_models():
 
 def test_training_finished():
     coordinator = Coordinator(required_participants=1, num_rounds=2)
-    coordinator.on_message(coordinator_pb2.RendezvousRequest(), "peer1")
+    coordinator.on_message(coordinator_pb2.RendezvousRequest(), "participant1")
 
     # Deliver results for 2 rounds
-    coordinator.on_message(coordinator_pb2.EndTrainingRequest(), "peer1")
-    coordinator.on_message(coordinator_pb2.EndTrainingRequest(), "peer1")
+    coordinator.on_message(coordinator_pb2.EndTrainingRequest(), "participant1")
+    coordinator.on_message(coordinator_pb2.EndTrainingRequest(), "participant1")
 
     assert coordinator.state == coordinator_pb2.State.FINISHED
 
@@ -132,42 +134,42 @@ def test_training_finished():
 def test_wrong_participant():
     # coordinator should not accept requests from participants that it has accepted
     coordinator = Coordinator(required_participants=1)
-    coordinator.on_message(coordinator_pb2.RendezvousRequest(), "peer1")
+    coordinator.on_message(coordinator_pb2.RendezvousRequest(), "participant1")
 
     with pytest.raises(UnknownParticipantError):
-        coordinator.on_message(coordinator_pb2.HeartbeatRequest(), "peer2")
+        coordinator.on_message(coordinator_pb2.HeartbeatRequest(), "participant2")
 
     with pytest.raises(UnknownParticipantError):
-        coordinator.on_message(coordinator_pb2.StartTrainingRequest(), "peer2")
+        coordinator.on_message(coordinator_pb2.StartTrainingRequest(), "participant2")
 
     with pytest.raises(UnknownParticipantError):
-        coordinator.on_message(coordinator_pb2.EndTrainingRequest(), "peer2")
+        coordinator.on_message(coordinator_pb2.EndTrainingRequest(), "participant2")
 
 
 def test_duplicated_update_submit():
     # the coordinator should not accept multiples updates from the same participant
     # in the same round
     coordinator = Coordinator(required_participants=2)
-    coordinator.on_message(coordinator_pb2.RendezvousRequest(), "peer1")
-    coordinator.on_message(coordinator_pb2.RendezvousRequest(), "peer2")
+    coordinator.on_message(coordinator_pb2.RendezvousRequest(), "participant1")
+    coordinator.on_message(coordinator_pb2.RendezvousRequest(), "participant2")
 
-    coordinator.on_message(coordinator_pb2.EndTrainingRequest(), "peer1")
+    coordinator.on_message(coordinator_pb2.EndTrainingRequest(), "participant1")
 
     with pytest.raises(DuplicatedUpdateError):
-        coordinator.on_message(coordinator_pb2.EndTrainingRequest(), "peer1")
+        coordinator.on_message(coordinator_pb2.EndTrainingRequest(), "participant1")
 
 
 def test_remove_participant():
     coordinator = Coordinator(required_participants=1)
-    coordinator.on_message(coordinator_pb2.RendezvousRequest(), "peer1")
+    coordinator.on_message(coordinator_pb2.RendezvousRequest(), "participant1")
 
     assert coordinator.state == coordinator_pb2.State.ROUND
 
-    coordinator.remove_participant("peer1")
+    coordinator.remove_participant("participant1")
 
     assert coordinator.participants.len() == 0
     assert coordinator.state == coordinator_pb2.State.STANDBY
 
-    coordinator.on_message(coordinator_pb2.RendezvousRequest(), "peer1")
+    coordinator.on_message(coordinator_pb2.RendezvousRequest(), "participant1")
 
     assert coordinator.state == coordinator_pb2.State.ROUND
