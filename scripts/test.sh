42f5656e1f7957711f4afd8565e7aaf34e026cd4
Anselmo Sampietro <anselmo.sampietro@xain.io>
2019-11-07 10:17:28 +0100

    bugfix, tell mypy to check for python files inside examples

--- scripts/test.sh
+++ scripts/test.sh
@@ -36,7 +36,7 @@ clang_format && echo "===> clang-format says: well done <===" &&
 pylint --rcfile=pylint.ini xain examples && echo "===> pylint says: well done <===" &&
 
 # type checks
-mypy --ignore-missing-imports xain examples && echo "===> mypy says: well done <===" &&
+mypy --ignore-missing-imports xain examples/* && echo "===> mypy says: well done <===" &&
 
 # documentation checks
 (cd docs/ && SPHINXOPTS="-W" make docs) && echo "===> sphinx-build says: well done <===" &&
