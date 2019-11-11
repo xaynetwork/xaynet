8035b5b51b6801d11ec8cc30e5dfa75f788a5bb3
Anselmo Sampietro <anselmo.sampietro@xain.io>
2019-11-11 17:40:14 +0100

    consistent capitalisation, using docstring

--- examples/tensorflow_hello_world/helloworld.py
+++ examples/tensorflow_hello_world/helloworld.py
@@ -18,17 +18,18 @@ flags.DEFINE_string("task_name", None, "")
 # We first define some constants for this specific example
 
 # Specify a dataset name for this example
-# This comes from the Fashion MNIST dataset:
-# https://research.zalando.com/welcome/mission/research-projects/fashion-mnist/
-# 100p means that it is split into 100 partitions, which are IID
+# We will use a partitioned version of the Fashion MNIST dataset
+# Please see here: https://xainag.github.io/xain/
+
+# 100p means that the dataset is split into 100 partitions, which are IID
 # Each partition represents the dataset a single client stores locally
 DATASET_NAME = "fashion-mnist-100p-iid-balanced"
 
-# standard attributes of the Fashion MNIST dataset
+# Standard attributes of the Fashion MNIST dataset
 NUM_CLASSES = 10
 INPUT_SHAPE = (28, 28, 1)
 
-# R is the number of global epochs the model is going to be trained for
+# R is the number of global rounds the model is going to be trained for
 R = 2
 
 # E is the number of local epochs
@@ -41,25 +42,21 @@ C = 0.02
 # B is the local batch size for a client update
 B = 10
 
-# default values for exponential decay learning rate
-DEFAULT_LR = 0.002
-DEFAULT_K = 0.01
-
 
 def main(_):
-    # fetch the data
-    # xy_train_partitions: each represent one local training data on a client
-    # xy_validation contains the global validation data (used for each participant)
-    # xy_test contains the global test data
+    # Fetch the data
+    # xy_train_partitions: Each partition represents the local training dataset of a single client
+    # xy_validation: Contains the global validation data (shared by all participants)
+    # xy_test: Contains the global test data (shared by all participants)
     xy_train_partitions, xy_validation, xy_test = load_splits(DATASET_NAME)
 
-    # declare model and compile Keras model
+    # Declare model architecture and compile Keras model
     model_fn = create_and_compile_model
 
-    # pass model and learning rate functions to ModelProvider
+    # Pass model and learning rate functions to ModelProvider
     model_provider = ModelProvider(model_fn=model_fn, lr_fn_fn=learning_rate_fn)
 
-    # init the Participant for each client.
+    # Init the Participant for each client.
     # At this stage they are not yet selected for training
     potential_participants = init_participants(
         xy_train_partitions=xy_train_partitions,
@@ -67,29 +64,38 @@ def main(_):
         xy_validation=xy_validation,
     )
 
-    # init the centralized Coordinator of the training
+    # Init the centralized Coordinator of the training
     coordinator = init_coordinator(
         participants=potential_participants,
-        xy_train_partitions=xy_train_partitions,
         model_provider=model_provider,
         xy_validation=xy_validation,
     )
 
-    # printing some basic statistics
+    # Printing some basic statistics
     print_dataset_stats(xy_train_partitions, xy_validation, xy_test)
 
-    # calling fit on the coordinator starts the training
-    # and print validation metrics after each global round
+    # Calling fit on the coordinator starts the training
+    # and prints validation metrics after R rounds of federated learning are completed
     validation_metrics, _, _, _ = coordinator.fit(num_rounds=R)
     print_validation_metrics(validation_metrics)
 
-    # evaluate the model with the test set
+    # Evaluate the model with the test set
     loss, accuracy = coordinator.evaluate(xy_test)
+
+    # Print final test loss and accuracy
     print(f"---\nTest completed!\nLoss: {loss} | Test accuracy: {accuracy}")
 
 
-# specify learning rate function, in this case with exponential decay
-def learning_rate_fn(epoch_base, lr_initial=DEFAULT_LR, k=DEFAULT_K):
+def learning_rate_fn(epoch_base, lr_initial=0.002, k=0.01):
+    """
+    Specify the learning rate function, in this case with exponential decay.
+
+    :param epoch_base: base epoch value
+    :param lr_initial: initial learning rate value
+    :param k: exponential decay constant
+    :return: decayed learning rate based on the epoch_optimizer
+    """
+
     def exp_decay(epoch_optimizer: int) -> float:
         epoch = epoch_base + epoch_optimizer
         return lr_initial * math.exp(-k * epoch)
@@ -97,8 +103,15 @@ def learning_rate_fn(epoch_base, lr_initial=DEFAULT_LR, k=DEFAULT_K):
     return exp_decay
 
 
-# this function contains the model architecture
 def create_and_compile_model(epoch_base=0):
+    """
+    This function contains the model architecture.
+    Once the architecture is specified, the model is compiled.
+
+    :param epoch_base: base epoch value
+    :return: a compiled tf.keras.Model instance, with exponential learning rate decay
+    """
+
     def add_convolution(filters, kernel_inizializer):
         convolution = Conv2D(
             filters,
@@ -112,7 +125,6 @@ def create_and_compile_model(epoch_base=0):
 
     ki = tf.keras.initializers.glorot_uniform(seed=42)
 
-    # Architecture
     inputs = Input(shape=INPUT_SHAPE)
     x = add_convolution(filters=32, kernel_inizializer=ki)(inputs)
     x = MaxPool2D(pool_size=(2, 2), strides=(2, 2))(x)
@@ -124,7 +136,6 @@ def create_and_compile_model(epoch_base=0):
 
     model = tf.keras.Model(inputs=inputs, outputs=outputs)
 
-    # Compile model with exponential learning rate decay
     lr_fn = learning_rate_fn(epoch_base=epoch_base)
     optimizer = tf.keras.optimizers.Adam(lr=lr_fn(0))
 
@@ -136,9 +147,15 @@ def create_and_compile_model(epoch_base=0):
     return model
 
 
-# iterate through each partition (a client's training data)
-# and init a Participant class
 def init_participants(xy_train_partitions, model_provider, xy_validation):
+    """
+    Iterate through each partition (a client's training data) and init a Participant class.
+
+    :param xy_train_partitions: partitioned training data
+    :param model_provider: ModelProvider instance holding model and learning rate functions
+    :param xy_validation: validation dataset
+    :return: list of initiated Participant instances, one per client
+    """
     potential_participants = []
     for client_id, xy_train in enumerate(xy_train_partitions):
         participant = Participant(
@@ -153,23 +170,31 @@ def init_participants(xy_train_partitions, model_provider, xy_validation):
     return potential_participants
 
 
-# init the Coordinator
-def init_coordinator(participants, xy_train_partitions, model_provider, xy_validation):
+def init_coordinator(participants, model_provider, xy_validation):
+    """
+    This function initiates the Coordinator.
 
-    # the controller will select the indices of the clients participating in the training
-    # right now it's initialized with all the indices of the clients available
+    The controller will select the indices of the clients participating in the training.
+    It is initialized with all the indices of the clients available.
+
+    Federated Averaging is a common aggregation method.
+    See here for more details: https://arxiv.org/pdf/1602.05629.pdf
+
+    The Coordinator will coordinate the training. It will:
+    - Select a C fraction of potential participants.
+    - Send training jobs to each selected Participant,
+    who will train its own local data for E local epochs.
+
+    :param participants: list of initiated Participant instances, one per client
+    :param model_provider: ModelProvider instance holding model and learning rate functions
+    :param xy_validation: validation dataset
+    :return: initiated Coordinator instance
+    """
     num_clients = len(participants)
-    assert num_clients == len(xy_train_partitions)
     controller = RandomController(num_clients)
 
-    # init the aggregator, Federated Averaging is the state-of-the-art aggregator
-    # see here for more details: https://arxiv.org/pdf/1602.05629.pdf
     aggregator = FederatedAveragingAgg()
 
-    # the Coordinator will coordinate the training. it will:
-    #   - select a C fraction of potential participants
-    #   - send training jobs to each selected Participant
-    #       who will train its own local data for E local epochs
     coordinator = Coordinator(
         controller=controller,
         model_provider=model_provider,
@@ -182,8 +207,15 @@ def init_coordinator(participants, xy_train_partitions, model_provider, xy_valid
     return coordinator
 
 
-# printing some preliminary stats to the terminal
 def print_dataset_stats(xy_train_partitions, xy_validation, xy_test):
+    """
+    This function prints some preliminary stats to the terminal.
+
+    :param xy_train_partitions: partitioned training data
+    :param xy_validation: validation dataset
+    :param xy_test: test dataset
+    :return: None
+    """
     print(f"\nthere are {len(xy_train_partitions)} client/potential partitions")
     images_first_client, labels_first_client = xy_train_partitions[0]
     n_images, height, width = images_first_client.shape
@@ -197,8 +229,13 @@ def print_dataset_stats(xy_train_partitions, xy_validation, xy_test):
     print(f"the test set is made of {len(test_images)} images\n---")
 
 
-# printing the validation metrics to the terminal
 def print_validation_metrics(validation_metrics):
+    """
+    Printing the validation metrics to the terminal.
+
+    :param validation_metrics: dictionary containing loss and accuracy per each round
+    :return: None
+    """
     for round_id in range(R):
         print(
             f"validation round: {round_id + 1}: "
