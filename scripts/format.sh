f5eb8ef5a33d53ba772f9e5f592edd565dad3aa8
Taner Topal <tanertopal42@gmail.com>
2019-11-19 11:17:13 +0100

    Apply formater to docs/conf.py

--- scripts/format.sh
+++ scripts/format.sh
@@ -7,6 +7,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 cd $DIR/../
 
-isort --indent=4 -rc setup.py conftest.py benchmarks examples xain
-black --exclude "xain/grpc/.*_pb2.*" setup.py conftest.py benchmarks examples xain
+isort --indent=4 -rc setup.py conftest.py benchmarks examples xain docs/conf.py
+black --exclude "xain/grpc/.*_pb2.*" setup.py conftest.py benchmarks examples xain docs/conf.py
 clang-format -style="{Language: Proto, BasedOnStyle: Google}" -i  protobuf/xain/grpc/*.proto
