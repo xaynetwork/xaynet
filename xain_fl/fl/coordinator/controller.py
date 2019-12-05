e2747efaf49c3178680f507f3fb3bc0561dcfed5
Anselmo Sampietro <anselmosampietro@XAINs-MBP.xain.internal>
2019-12-05 16:01:10 +0100

    XP-238 remove legacy controllers, create test, refactor local usage of RandomController

--- xain_fl/fl/coordinator/controller.py
+++ xain_fl/fl/coordinator/controller.py
@@ -22,18 +22,18 @@ class Controller(ABC):
     def __init__(self, participants_ids: List[str], fraction_of_participants: float = 1.0) -> None:
         self.participants_ids = participants_ids
         self.fraction_of_participants = fraction_of_participants
+        self.num_ids_to_select = self.get_num_ids_to_select()
 
-    # TODO: make this a property?
     def get_num_ids_to_select(self) -> int:
         """Calculates how many participant ids need to be selected.
 
         Returns:
             :obj:`int`: Number of participant ids to be selected
         """
-
         raw_num_ids_to_select = len(self.participants_ids) * self.fraction_of_participants
-        ceiling = np.ceil(raw_num_ids_to_select)
-        return int(ceiling)
+        max_valid_value = max(1, np.ceil(raw_num_ids_to_select))
+        minimum_valid_value = min(len(self.participants_ids), max_valid_value)
+        return int(minimum_valid_value)
 
     @abstractmethod
     def select_ids(self) -> List[str]:
@@ -49,35 +49,8 @@ class RandomController(Controller):
     """Generates a random sample of the provided ids of size num_ids_to_select"""
 
     def select_ids(self) -> List[str]:
-        num_ids_to_select = self.get_num_ids_to_select()
-        return np.random.choice(self.participants_ids, size=num_ids_to_select, replace=False)
-
-
-# TODO: refactor, legacy code
-class RoundRobinController(Controller):
-    """Cycles through all indicies in an ordered fashion."""
-
-    def __init__(self, num_participants: int) -> None:
-        super().__init__(num_participants)
-        self.next_index: int = 0
-
-    def select_ids(self) -> List[str]:
-        next_index = self.next_index
-        self.next_index = (next_index + 1) % self.num_participants
-        return [next_index]
-
-
-# TODO: refactor, legacy code
-class CycleRandomController(Controller):
-    """Cycles through all indicies in a random fashion."""
-
-    def __init__(self, num_participants: int) -> None:
-        super().__init__(num_participants)
-        self.cycle: np.ndarray = np.array([])
-
-    def select_ids(self) -> List[str]:
-        if self.cycle.size == 0:
-            self.cycle = np.random.permutation(self.num_participants)
-        next_index = self.cycle[0]
-        self.cycle = self.cycle[1:]
-        return [next_index]
+        return np.random.choice(
+            self.participants_ids,
+            size=self.num_ids_to_select,
+            replace=False,
+        )
