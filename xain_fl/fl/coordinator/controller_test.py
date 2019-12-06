47ba706925d1870ebb5d69c9e63bc56a7e6bc9ec
Anselmo Sampietro <anselmosampietro@XAINs-MBP.xain.internal>
2019-12-06 14:34:40 +0100

    XP-238 update controller test, add new coordinator tests

--- xain_fl/fl/coordinator/controller_test.py
+++ xain_fl/fl/coordinator/controller_test.py
@@ -4,13 +4,15 @@ from xain_fl.fl.coordinator.controller import RandomController
 
 
 def test_random_controller():
-    """Tests that the length of selected ids equals num_ids_to_select
-    and that there's no replacement.
+    """Tests that the length of selected ids is correct and that
+    there's no replacement.
     """
     participant_ids = ["a", "b", "c", "d", "e", "f", "g"]
-    for fraction in np.arange(0.25, 1, 0.25):
+    fractions = np.arange(0.25, 1, 0.25)
+    len_ids_selected = [np.ceil(fraction * len(participant_ids)) for fraction in fractions]
+    for fraction, len_ids in zip(fractions, len_ids_selected):
         controller = RandomController(participant_ids, fraction_of_participants=fraction)
         ids = controller.select_ids()
 
-        assert len(ids) == controller.num_ids_to_select
+        assert len(ids) == len_ids
         assert len(set(ids)) == len(ids)
