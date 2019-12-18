6ce21180a3edcf7ea40aa9688c62ad8fd7cd5892
Felix Reichel <felix.reichel@xain.io>
2019-12-18 17:18:03 +0100

    XP-265 remove references to either examples or benchmark directory

--- scripts/format.sh
+++ scripts/format.sh
@@ -7,5 +7,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 cd $DIR/../
 
-isort --indent=4 -rc setup.py conftest.py benchmarks examples xain_fl docs/conf.py
-black --exclude "xain_fl/cproto/.*_pb2.*" setup.py conftest.py benchmarks examples xain_fl docs/conf.py
+isort --indent=4 -rc setup.py conftest.py xain_fl docs/conf.py
+black --exclude "xain_fl/cproto/.*_pb2.*" setup.py conftest.py xain_fl docs/conf.py
