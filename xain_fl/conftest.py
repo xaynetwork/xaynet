11b5f08a443cb0f3c082d9ce3af6f34c1451cb57
Felix Reichel <felix.reichel@xain.io>
2019-12-20 09:48:52 +0100

    XP-243 remove flags from xain-fl codebase

--- xain_fl/conftest.py
+++ xain_fl/conftest.py
@@ -1,20 +1,4 @@
-from absl import flags
-
-FLAGS = flags.FLAGS
-
-
 def pytest_collection_modifyitems(items):
     for item in items:
         if not any(item.iter_markers()):
             item.add_marker("unmarked")
-
-
-def pytest_runtest_setup():
-    # Invoking FLAGS will make the flags usable for the
-    # test execution and avoid throwing an error
-    FLAGS(
-        argv=[
-            "test",  # some app name required
-            "--fetch_datasets=True",  # resetting to default at beginning of every test
-        ]
-    )
