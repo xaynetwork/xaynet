efe3db62b9c597fbb5393f004b58fcf1d4004c81
Corentin Henry <corentinhenry@gmail.com>
2020-01-07 09:53:55 +0100

    script/format.sh: print the commands being run
    The commands in this script can make changes to the repo, so users may
    appreciate seeing the actual commands being run.

--- scripts/format.sh
+++ scripts/format.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-# Exit immediately if a command exits with a non-zero status.
+# Exit immediately if a command exits with a non-zero status
 set -e
-
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
 cd $DIR/../
 
+set -x
 isort --indent=4 -rc setup.py conftest.py xain_fl docs/conf.py
 black --exclude "xain_fl/cproto/.*_pb2.*" setup.py conftest.py xain_fl docs/conf.py
