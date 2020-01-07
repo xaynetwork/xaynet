98ebe2bc886bf0a014509199f5dff04284e33127
Corentin Henry <corentinhenry@gmail.com>
2020-01-07 10:06:24 +0100

    XP-273: make scripts/rm_cache.sh less aggressive
    - print the `rm` commands being run so that users know what's being deleted
    - do not delete the `__pycache` directories so aggressively. For
      instance I usually create a virtualenv in each projects I'm
      working on and don't want to have a script touching it.

--- scripts/rm_caches.sh
+++ scripts/rm_caches.sh
@@ -3,8 +3,10 @@ set -e  # Exit immediately if a command exits with a non-zero status
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $DIR/../
 
+set -x
 rm -rf .mypy_cache
 rm -rf .pytest_cache
-find . -type d -name __pycache__ -exec rm -r {} \+
+rm -rf __pycache__
+find xain_fl tests -type d -name __pycache__ -exec rm -r {} \+
 rm -rf docs/_code_reference_*
 rm -rf docs/_build
