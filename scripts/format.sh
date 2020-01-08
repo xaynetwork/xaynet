e6a7a05e98e1a9528086d11848f959ee9ad87ba2
Anselmo Sampietro <anselmo.sampietro@xain.io>
2020-01-08 16:19:30 +0100

    XP-384 remove unused files (#209)
    * XP-384 remove unused files
    
    * XP-384 update CI jobs

--- scripts/format.sh
+++ scripts/format.sh
@@ -6,5 +6,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $DIR/../
 
 set -x
-isort --indent=4 -rc setup.py conftest.py xain_fl docs/conf.py
-black setup.py conftest.py xain_fl docs/conf.py
+isort --indent=4 -rc setup.py xain_fl docs/conf.py
+black setup.py xain_fl docs/conf.py
--- scripts/test.sh
+++ scripts/test.sh
@@ -5,10 +5,10 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $DIR/../
 
 # sort import
-isort --check-only --indent=4 -rc setup.py conftest.py xain_fl && echo "===> isort says: well done <===" &&
+isort --check-only --indent=4 -rc setup.py xain_fl && echo "===> isort says: well done <===" &&
 
 # format code
-black --check setup.py conftest.py xain_fl && echo "===> black says: well done <===" &&
+black --check setup.py xain_fl && echo "===> black says: well done <===" &&
 
 # lint
 pylint --rcfile=pylint.ini xain_fl && echo "===> pylint says: well done <===" &&
