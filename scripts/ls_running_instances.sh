29548db2aa3496c477b9ec8654b73187fa93995b
Corentin Henry <little-dude@users.noreply.github.com>
2020-01-08 11:51:36 +0100

    Xp 273 scripts cleanup (#206)
    * XP-273: remove scripts/test_slow.sh
    
    This script is basically a one-liner (`pytest --run-slow`), and is
    effectively a noop since there is no test marked as slow.
    
    * XP-273: remove scripts/setup{_pyenv}.sh
    
    setup_pyenv.sh suffers multiple issues that justify its removal:
    
    - it is very intrusive: it downloads software in the user home
      directory, edits their zshrc/bashrc and installs software using the
      system package manager which requires root permissions
    - it can break the user configuration if they already have a pyenv
      installation
    - it only works in very specific cases: if the user uses a Debian
      based distro with bash as a default shell or MacOS with zsh
    
    setup.sh is not particularly useful because most python developers
    will do `pip install -e .[dev]` by themself.
    
    * XP-273: script/format.sh: print the commands
    
    The commands in this script can make changes to the repo, so users may
    appreciate seeing the actual commands being run.
    
    * XP-273: make scripts/rm_cache.sh less aggressive
    
    - print the `rm` commands being run so that users know what's being deleted
    - do not delete the `__pycache` directories so aggressively. For
      instance I usually create a virtualenv in each projects I'm
      working on and don't want to have a script touching it.
    
    * XP-273: remove scripts/ls_running_instances.sh
    
    This script requires private credentials to run and is specific to our
    infrastructure so it does not make sense to keep it public.

--- scripts/ls_running_instances.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e  # Exit immediately if a command exits with a non-zero status
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-cd $DIR/../
-
-AWS_PROFILE=xain-xain aws ec2 describe-instances  --filters Name=instance-state-code,Values=16 | jq '.Reservations[].Instances[] | "\(.Tags[].Value), \(.PublicIpAddress)"'
