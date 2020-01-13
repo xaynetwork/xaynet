2122bb603dbe533557f7f375c6b38a9681fd9dca
Corentin Henry <corentinhenry@gmail.com>
2020-01-13 18:44:36 +0100

    XP-308: Add unit test doing an entire round
    Refs:
    
    https://xainag.atlassian.net/browse/XP-40
    https://xainag.atlassian.net/browse/XP-308
    https://xainag.atlassian.net/browse/XP-309
    https://xainag.atlassian.net/browse/XP-311
    
    Summary:
    
    This is the first test where multiple participants connect and do a
    round of training.
    
    Implementation notes:
    
    - Because we use the peer address to identify a participant, we have to
      use a port forwarding trick to make distinct connections to the
      coordinator. This is implemented in the `tests.port_forwarding` module
    
    - To test the store implementation, we use a custom TestStore class that
      writes data in memory instead of using an actual S3 bucket. That way
      our tests don't depend on an external service. This is implemented in
      the `tests.store` module.

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,6 +10,9 @@ from xain_fl.coordinator.coordinator import Coordinator
 from xain_fl.coordinator.coordinator_grpc import CoordinatorGrpc
 from xain_fl.helloproto.numproto_server import NumProtoServer
 
+from .port_forwarding import ConnectionManager
+from .store import TestStore
+
 
 @pytest.fixture
 def greeter_server():
@@ -35,7 +38,8 @@ def coordinator_service():
 
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=1))
     coordinator = Coordinator(minimum_participants_in_round=10, fraction_of_participants=1.0)
-    coordinator_grpc = CoordinatorGrpc(coordinator)
+    store = TestStore()
+    coordinator_grpc = CoordinatorGrpc(coordinator, store)
     coordinator_pb2_grpc.add_CoordinatorServicer_to_server(coordinator_grpc, server)
     server.add_insecure_port("localhost:50051")
     server.start()
@@ -59,3 +63,41 @@ def participant_stub():
     yield stub
 
     channel.close()
+
+
+def port_generator():
+    """A generator that yields incrementing port numbers.
+
+    """
+    port = 50052
+    while True:
+        yield port
+        port += 1
+
+
+@pytest.fixture
+def participant_stubs():
+    """Generator that yields functions instantiate participant stubs.
+    Each participant creates a new TCP connection, so that they get a
+    different participant ID.
+
+    """
+
+    ports = port_generator()
+    connections = ConnectionManager()
+    channels = []
+
+    def generate_participant_stubs():
+        for port in ports:
+            connections.start("localhost", port, "localhost", 50051)
+            channel = grpc.insecure_channel(f"localhost:{port}")
+            channels.append(channel)
+            stub = coordinator_pb2_grpc.CoordinatorStub(channel)
+            yield stub
+
+    yield generate_participant_stubs()
+
+    for channel in channels:
+        channel.close()
+
+    connections.stop_all()
