eb71d136c4a72d45acb71ca0a241b82d657974ad
Jan Petsche <58227040+janpetschexain@users.noreply.github.com>
2020-01-20 16:58:32 +0100

    XP-510 allow for zero epochs on cli

--- xain_fl/cli.py
+++ xain_fl/cli.py
@@ -57,7 +57,7 @@ def type_num_epochs(value):
 
     ivalue = int(value)
 
-    if ivalue <= 0:
+    if ivalue < 0:
         raise argparse.ArgumentTypeError("%s is an invalid positive int value" % value)
 
     if ivalue > 10_000:
