3b83135f75e60c24837c3fe4f1b5d7271b481208
Jan Petsche <58227040+janpetschexain@users.noreply.github.com>
2020-02-03 14:42:15 +0100

    PB-366 update dependencies

--- setup.py
+++ setup.py
@@ -50,7 +50,6 @@ tests_require = [
     "pytest==5.3.2",  # MIT license
     "pytest-cov==2.8.1",  # MIT
     "pytest-watch==4.2.0",  # MIT
-    "xain-sdk==0.3.0",  # Apache License 2.0
     "xain-sdk @ git+https://github.com/xainag/xain-sdk.git@development#egg=xain_sdk-0.3.0",  # Apache License 2.0
 ]
 
