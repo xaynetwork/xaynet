940f755fd3368e2c15798b56f5b9b9ab3a11181f
Robert Steiner <robertt.debug@gmail.com>
2020-03-24 18:02:32 +0100

    Upgrade influxdb

--- rust/src/common/metric_store/influxdb.rs
+++ rust/src/common/metric_store/influxdb.rs
@@ -2,14 +2,34 @@ use chrono::{DateTime, Utc};
 use influxdb::{Client, InfluxDbWriteable, Timestamp};
 use tokio::sync::mpsc::{unbounded_channel, UnboundedReceiver, UnboundedSender};
 
+pub enum Measurement {
+    Round(RoundMeasurement),
+    Counter(CounterMeasurement),
+}
+
 #[derive(InfluxDbWriteable)]
-struct RoundMeasurement {
+pub struct RoundMeasurement {
     time: DateTime<Utc>,
     round: u32,
 }
 
+impl RoundMeasurement {
+    pub fn new(round: u32) -> RoundMeasurement {
+        RoundMeasurement {
+            time: Timestamp::Now.into(),
+            round,
+        }
+    }
+}
+
+impl From<RoundMeasurement> for Measurement {
+    fn from(value: RoundMeasurement) -> Self {
+        Self::Round(value)
+    }
+}
+
 #[derive(InfluxDbWriteable)]
-struct CounterMeasurement {
+pub struct CounterMeasurement {
     time: DateTime<Utc>,
     number_of_selected_participants: u32,
     number_of_waiting_participants: u32,
@@ -18,28 +38,50 @@ struct CounterMeasurement {
     number_of_ignored_participants: u32,
 }
 
-trait Metric {
-    fn to_measurement(&self) -> Box<dyn InfluxDbWriteable>;
+impl CounterMeasurement {
+    pub fn new(
+        number_of_selected_participants: u32,
+        number_of_waiting_participants: u32,
+        number_of_done_participants: u32,
+        number_of_done_inactive_participants: u32,
+        number_of_ignored_participants: u32,
+    ) -> CounterMeasurement {
+        CounterMeasurement {
+            time: Timestamp::Now.into(),
+            number_of_selected_participants,
+            number_of_waiting_participants,
+            number_of_done_participants,
+            number_of_done_inactive_participants,
+            number_of_ignored_participants,
+        }
+    }
 }
 
-pub async fn run_metricstore(mut influx_connector: InfluxDBConnector) {
+impl From<CounterMeasurement> for Measurement {
+    fn from(value: CounterMeasurement) -> Self {
+        Self::Counter(value)
+    }
+}
+
+pub async fn run_metricstore(mut influxdb_connector: InfluxDBConnector) {
     loop {
-        match influx_connector.receiver.recv().await {
-            Some(t) => {
-                // let mut write_query: WriteQuery =
-                //     Query::write_query(Timestamp::Now, metric_owner.to_string());
-
-                // for (name, value) in fields {
-                //     write_query = write_query.add_field(name, value);
-                // }
-
-                // // Submit the query to InfluxDB.
-                // let _ = influx_connector
-                //     .client
-                //     .query(&write_query)
-                //     .await
-                //     .map_err(|e| error!("{}", e));
-            }
+        match influxdb_connector.receiver.recv().await {
+            Some(measurement) => match measurement {
+                Measurement::Round(round) => {
+                    influxdb_connector
+                        .client
+                        .query(&round.into_query("coordinator"))
+                        .await
+                        .map_err(|e| eprintln!("{}", e));
+                }
+                Measurement::Counter(counter) => {
+                    influxdb_connector
+                        .client
+                        .query(&counter.into_query("coordinator"))
+                        .await
+                        .map_err(|e| eprintln!("{}", e));
+                }
+            },
             None => {
                 warn!("All senders have been dropped!");
                 return;
@@ -48,35 +90,13 @@ pub async fn run_metricstore(mut influx_connector: InfluxDBConnector) {
     }
 }
 
-// pub enum MetricOwner {
-//     Coordinator,
-//     Participant,
-// }
-
-// impl From<&MetricOwner> for &'static str {
-//     fn from(metric_owner: &MetricOwner) -> &'static str {
-//         match metric_owner {
-//             MetricOwner::Coordinator => "coordinator",
-//             MetricOwner::Participant => "participant",
-//         }
-//     }
-// }
-
-// impl ToString for MetricOwner {
-//     fn to_string(&self) -> String {
-//         Into::<&str>::into(self).into()
-//     }
-// }
-
-// pub struct Metric(pub MetricOwner, pub Vec<(&'static str, Type)>);
-
 pub struct InfluxDBConnector {
     client: Client,
-    receiver: UnboundedReceiver<Box<dyn Metric>>,
+    receiver: UnboundedReceiver<Measurement>,
 }
 
 impl InfluxDBConnector {
-    pub fn new(host: &str, db_name: &str) -> (InfluxDBConnector, UnboundedSender<Box<dyn Metric>>) {
+    pub fn new(host: &str, db_name: &str) -> (InfluxDBConnector, UnboundedSender<Measurement>) {
         let (sender, receiver) = unbounded_channel();
         (
             InfluxDBConnector {
