3eef50097957aeef7ebdab66ede6a9748d29e640
Robert Steiner <robertt.debug@gmail.com>
2020-04-20 16:58:01 +0200

    Add get methods

--- rust/src/storage/append/redis.rs
+++ rust/src/storage/append/redis.rs
@@ -1,8 +1,18 @@
 use crate::{
-    coordinator::Coordinator,
-    storage::append::types::{CoordinatorPartialState, MaskDictEntry, SeedDictEntry, SumDictEntry},
+    coordinator::{
+        Coordinator, EncryptedMaskingSeed, SeedDict, SumDict, SumParticipantEphemeralPublicKey,
+        SumParticipantPublicKey, UpdateParticipantPublicKey,
+    },
+    storage::append::types::{
+        CoordinatorPartialState, CoordinatorPartialStateResult, MaskDictEntry, SeedDictEntry,
+        SeedDictEntryResult, SeedDictKeyResult, SumDictEntry, SumDictResult,
+    },
 };
-use redis::{aio::MultiplexedConnection, AsyncCommands, Client, RedisError};
+use redis::{
+    aio::MultiplexedConnection, AsyncCommands, Client, ErrorKind, FromRedisValue, RedisError,
+    RedisResult,
+};
+use std::{collections::HashMap, convert::TryFrom};
 
 #[derive(Clone)]
 pub struct RedisStore {
@@ -19,21 +29,21 @@ impl RedisStore {
 
     async fn set_partial_coordinator_state(
         &mut self,
-        coordinator: &Coordinator,
-    ) -> Result<(), RedisError> {
+        partial_state: CoordinatorPartialState,
+    ) -> RedisResult<()> {
         self.connection
-            .set_multiple(&CoordinatorPartialState::from(coordinator).to_args())
+            .set_multiple(&partial_state.to_args())
             .await?;
         Ok(())
     }
 
-    async fn set_sum_dict_entry(&mut self, entry: SumDictEntry) -> Result<(), RedisError> {
+    async fn set_sum_dict_entry(&mut self, entry: SumDictEntry) -> RedisResult<()> {
         let entry = entry.to_args();
         self.connection.hset("sum_dict", entry.0, entry.1).await?;
         Ok(())
     }
 
-    async fn set_seed_dict_entry(&mut self, entry: SeedDictEntry) -> Result<(), RedisError> {
+    async fn set_seed_dict_entry(&mut self, entry: SeedDictEntry) -> RedisResult<()> {
         let entry = entry.to_args();
         self.connection.sadd("seed_dict", &entry.0).await?;
         self.connection
@@ -42,12 +52,45 @@ impl RedisStore {
         Ok(())
     }
 
-    async fn set_mask_dict_entry(&mut self, entry: MaskDictEntry) -> Result<(), RedisError> {
+    async fn set_mask_dict_entry(&mut self, entry: MaskDictEntry) -> RedisResult<()> {
         self.connection.sadd("mask_dict", &entry.to_args()).await?;
         Ok(())
     }
 
-    async fn schedule_snapshot(&mut self) -> Result<(), RedisError> {
+    async fn get_partial_coordinator_state(
+        &mut self,
+    ) -> Result<CoordinatorPartialState, Box<dyn std::error::Error + 'static>> {
+        let result = CoordinatorPartialStateResult(
+            self.connection.get(CoordinatorPartialState::keys()).await?,
+        );
+
+        Ok(CoordinatorPartialState::try_from(result)?)
+    }
+
+    async fn get_sum_dict(&mut self) -> Result<SumDict, Box<dyn std::error::Error + 'static>> {
+        let result: SumDictResult = SumDictResult(self.connection.hgetall("sum_dict").await?);
+        Ok(SumDict::try_from(result)?)
+    }
+
+    async fn get_seed_dict(&mut self) -> Result<SeedDict, Box<dyn std::error::Error + 'static>> {
+        let seed_dict_keys: Vec<String> = self.connection.smembers("seed_dict").await?;
+
+        let mut seed_dict: SeedDict = HashMap::new();
+        for sum_key in seed_dict_keys.into_iter() {
+            let seed_dict_fields: Vec<(String, String)> = self
+                .connection
+                .hgetall(format!("seed_dict:{}", &sum_key))
+                .await?;
+            let sub_dict: HashMap<SumParticipantPublicKey, EncryptedMaskingSeed> =
+                HashMap::try_from(SeedDictEntryResult(seed_dict_fields))?;
+            let key = SumParticipantPublicKey::try_from(SeedDictKeyResult(sum_key))?;
+            seed_dict.insert(key, sub_dict);
+        }
+
+        Ok(seed_dict)
+    }
+
+    async fn schedule_snapshot(&mut self) -> RedisResult<()> {
         redis::cmd("BGSAVE")
             .arg("SCHEDULE")
             .query_async(&mut self.connection)
@@ -55,7 +98,7 @@ impl RedisStore {
         Ok(())
     }
 
-    async fn clear_all(&mut self) -> Result<(), RedisError> {
+    async fn clear_all(&mut self) -> RedisResult<()> {
         redis::cmd("FLUSHDB")
             .arg("ASYNC")
             .query_async(&mut self.connection)
@@ -66,7 +109,14 @@ impl RedisStore {
 
 #[cfg(test)]
 mod tests {
-    use crate::coordinator::{Coordinator, EncryptedMaskingSeed, UpdateParticipantPublicKey};
+    use super::RedisStore;
+    use crate::{
+        coordinator::{
+            Coordinator, EncryptedMaskingSeed, SeedDict, SumDict, SumParticipantEphemeralPublicKey,
+            SumParticipantPublicKey, UpdateParticipantPublicKey,
+        },
+        storage::append::types::*,
+    };
     use redis::RedisResult;
     use sodiumoxide::{crypto::box_, randombytes::randombytes};
     use std::{collections::HashMap, time::Instant};
@@ -76,51 +126,72 @@ mod tests {
         // create new coordinator
         let coordinator = Coordinator::new().unwrap();
         let mut store = RedisStore::new("redis://127.0.0.1/").await.unwrap();
+
+        store.clear_all().await.unwrap();
+
         store
-            .set_partial_coordinator_state(&coordinator)
+            .set_partial_coordinator_state(CoordinatorPartialState::from(&coordinator))
             .await
             .unwrap();
 
-        let (k, _) = box_::gen_keypair();
-        store.set_sum_dict_entry(SumDictEntry(k, k)).await.unwrap();
+        let read = store.get_partial_coordinator_state().await.unwrap();
+        println!("Coordinator: {:?}", read);
 
-        let (k, _) = box_::gen_keypair();
-        let mut sub_dict: HashMap<UpdateParticipantPublicKey, EncryptedMaskingSeed> =
-            HashMap::new();
-        sub_dict.insert(k, randombytes(80));
+        let sum_participant_pk = box_::PublicKey([0_u8; box_::PUBLICKEYBYTES]);
+        let sum_participant_epk = box_::PublicKey([0_u8; box_::PUBLICKEYBYTES]);
+        let mut result: SumDict = HashMap::new();
+        result.insert(sum_participant_pk, sum_participant_epk);
+
         store
-            .set_seed_dict_entry(SeedDictEntry(k, sub_dict))
+            .set_sum_dict_entry(SumDictEntry(sum_participant_pk, sum_participant_epk))
             .await
             .unwrap();
+        let sum_dict = store.get_sum_dict().await.unwrap();
+
+        println!("Sum dict: {:?}", &sum_dict);
+        assert_eq!(result, sum_dict);
+
+        let mut result_sub: HashMap<SumParticipantPublicKey, EncryptedMaskingSeed> = HashMap::new();
+        result_sub.insert(sum_participant_pk, randombytes(80));
+        let mut result: SeedDict = HashMap::new();
+        result.insert(sum_participant_pk, result_sub.clone());
+
+        store
+            .set_seed_dict_entry(SeedDictEntry(sum_participant_pk, result_sub))
+            .await
+            .unwrap();
+
+        let seed_dict = store.get_seed_dict().await.unwrap();
+        println!("Seed dict: {:?}", seed_dict);
     }
 
-    #[tokio::test]
-    async fn test_10k_loop() {
-        let mut store = RedisStore::new("redis://127.0.0.1/").await.unwrap();
+    // #[tokio::test]
+    // async fn test_10k_loop() {
+    //     let mut store = RedisStore::new("redis://127.0.0.1/").await.unwrap();
 
-        let now = Instant::now();
-        for _ in 0..10_000 {
-            let (k, _) = box_::gen_keypair();
-            store.set_sum_dict_entry(SumDictEntry(k, k)).await.unwrap();
-        }
-        let new_now = Instant::now();
-        println!("Add sum dict keys {:?}", new_now.duration_since(now));
-    }
+    //     let now = Instant::now();
+    //     for _ in 0..10_000 {
+    //         let (k, _) = box_::gen_keypair();
+    //         store.set_sum_dict_entry(SumDictEntry(k, k)).await.unwrap();
+    //     }
+    //     let new_now = Instant::now();
+    //     println!("Add sum dict keys {:?}", new_now.duration_since(now));
+    // }
 
-    #[tokio::test]
-    async fn test_10k_join() {
-        let store = RedisStore::new("redis://127.0.0.1/").await.unwrap();
+    // #[tokio::test]
+    // async fn test_10k_join() {
+    //     let store = RedisStore::new("redis://127.0.0.1/").await.unwrap();
 
-        async fn create(r: &RedisStore) -> RedisResult<()> {
-            let mut red = r.clone();
-            let (k, _) = box_::gen_keypair();
-            red.set_sum_dict_entry(SumDictEntry(k, k)).await
-        }
-        let cmds = (0..10_000).map(|_| create(&store));
+    //     async fn create(r: &RedisStore) -> RedisResult<()> {
+    //         let mut red = r.clone();
+    //         let (k, _) = box_::gen_keypair();
+    //         red.set_sum_dict_entry(SumDictEntry(k, k)).await
+    //     }
+    //     let cmds = (0..10_000).map(|_| create(&store));
 
-        let now = Instant::now();
-        let _ = future::try_join_all(cmds).await.unwrap();
-        let new_now = Instant::now();
-        println!("Add sum dict keys {:?}", new_now.duration_since(now));
-    }
+    //     let now = Instant::now();
+    //     let _ = future::try_join_all(cmds).await.unwrap();
+    //     let new_now = Instant::now();
+    //     println!("Add sum dict keys {:?}", new_now.duration_since(now));
+    // }
 }
--- rust/src/storage/append/types.rs
+++ rust/src/storage/append/types.rs
@@ -1,9 +1,13 @@
-use crate::coordinator::Coordinator;
+use crate::coordinator::{
+    Coordinator, EncryptedMaskingSeed, SumDict, SumParticipantEphemeralPublicKey,
+    SumParticipantPublicKey,
+};
 use data_encoding::HEXUPPER;
 use redis::ToRedisArgs;
 use sodiumoxide::crypto::box_;
-use std::collections::HashMap;
+use std::{collections::HashMap, convert::TryFrom, str::FromStr};
 
+#[derive(Debug)]
 pub struct CoordinatorPartialState {
     encr_pk: String,
     encr_sk: String,
@@ -16,6 +20,22 @@ pub struct CoordinatorPartialState {
     min_update: usize,
     phase: String,
 }
+
+#[derive(Debug)]
+pub struct CoordinatorPartialStateResult(
+    pub  (
+        Option<String>,
+        Option<String>,
+        Option<String>,
+        Option<String>,
+        Option<String>,
+        Option<String>,
+        Option<String>,
+        Option<usize>,
+        Option<usize>,
+        Option<String>,
+    ),
+);
 
 impl CoordinatorPartialState {
     pub fn to_args(&self) -> Vec<(impl ToRedisArgs, impl ToRedisArgs)> {
@@ -30,6 +50,21 @@ impl CoordinatorPartialState {
             ("min_sum", self.min_sum.to_string()),
             ("min_update", self.min_update.to_string()),
             ("phase", self.phase.clone()),
+        ]
+    }
+
+    pub fn keys() -> Vec<&'static str> {
+        vec![
+            "encr_pk",
+            "encr_sk",
+            "sign_pk",
+            "sign_sk",
+            "sum",
+            "update",
+            "seed",
+            "min_sum",
+            "min_update",
+            "phase",
         ]
     }
 }
@@ -51,7 +86,25 @@ impl From<&Coordinator> for CoordinatorPartialState {
     }
 }
 
-pub struct SumDictEntry(box_::PublicKey, box_::PublicKey);
+impl TryFrom<CoordinatorPartialStateResult> for CoordinatorPartialState {
+    type Error = Box<dyn std::error::Error + 'static>;
+    fn try_from(result: CoordinatorPartialStateResult) -> Result<Self, Self::Error> {
+        Ok(Self {
+            encr_pk: (result.0).0.ok_or(StoreError::Read)?,
+            encr_sk: (result.0).1.ok_or(StoreError::Read)?,
+            sign_pk: (result.0).2.ok_or(StoreError::Read)?,
+            sign_sk: (result.0).3.ok_or(StoreError::Read)?,
+            sum: f64::from_str(&(result.0).4.ok_or(StoreError::Read)?)?,
+            update: f64::from_str(&(result.0).5.ok_or(StoreError::Read)?)?,
+            seed: (result.0).6.ok_or(StoreError::Read)?,
+            min_sum: (result.0).7.ok_or(StoreError::Read)?,
+            min_update: (result.0).8.ok_or(StoreError::Read)?,
+            phase: (result.0).9.ok_or(StoreError::Read)?,
+        })
+    }
+}
+
+pub struct SumDictEntry(pub box_::PublicKey, pub box_::PublicKey);
 
 impl SumDictEntry {
     pub fn to_args(&self) -> (String, String) {
@@ -62,7 +115,24 @@ impl SumDictEntry {
     }
 }
 
-pub struct SeedDictEntry(box_::PublicKey, HashMap<box_::PublicKey, Vec<u8>>);
+pub struct SumDictResult(pub Vec<(String, String)>);
+
+impl TryFrom<SumDictResult> for SumDict {
+    type Error = Box<dyn std::error::Error + 'static>;
+    fn try_from(result: SumDictResult) -> Result<Self, Self::Error> {
+        let mut sum_dict: SumDict = HashMap::new();
+        for (k, v) in result.0.iter() {
+            let new_k = box_::PublicKey::from_slice(HEXUPPER.decode(&k.as_bytes())?.as_slice())
+                .ok_or(StoreError::Read)?;
+            let new_v = box_::PublicKey::from_slice(HEXUPPER.decode(&v.as_bytes())?.as_slice())
+                .ok_or(StoreError::Read)?;
+            sum_dict.insert(new_k, new_v);
+        }
+        Ok(sum_dict)
+    }
+}
+
+pub struct SeedDictEntry(pub box_::PublicKey, pub HashMap<box_::PublicKey, Vec<u8>>);
 
 impl SeedDictEntry {
     pub fn to_args(&self) -> (String, Vec<(String, String)>) {
@@ -75,10 +145,68 @@ impl SeedDictEntry {
     }
 }
 
-pub struct MaskDictEntry(Vec<u8>);
+pub struct SeedDictEntryResult(pub Vec<(String, String)>);
+
+impl TryFrom<SeedDictEntryResult> for HashMap<SumParticipantPublicKey, EncryptedMaskingSeed> {
+    type Error = Box<dyn std::error::Error + 'static>;
+    fn try_from(result: SeedDictEntryResult) -> Result<Self, Self::Error> {
+        let mut sub_dict: HashMap<SumParticipantPublicKey, EncryptedMaskingSeed> = HashMap::new();
+        for (k, v) in result.0.iter() {
+            let new_k = box_::PublicKey::from_slice(HEXUPPER.decode(&k.as_bytes())?.as_slice())
+                .ok_or(StoreError::Read)?;
+            let new_v = HEXUPPER.decode(&v.as_bytes())?;
+            sub_dict.insert(new_k, new_v);
+        }
+        Ok(sub_dict)
+    }
+}
+
+pub struct SeedDictKeyResult(pub String);
+
+impl TryFrom<SeedDictKeyResult> for SumParticipantPublicKey {
+    type Error = Box<dyn std::error::Error + 'static>;
+    fn try_from(result: SeedDictKeyResult) -> Result<Self, Self::Error> {
+        let key = box_::PublicKey::from_slice(HEXUPPER.decode(&result.0.as_bytes())?.as_slice())
+            .ok_or(StoreError::Read)?;
+        Ok(key)
+    }
+}
+
+pub struct MaskDictEntry(pub Vec<u8>);
 
 impl MaskDictEntry {
     pub fn to_args(&self) -> String {
         HEXUPPER.encode(&self.0.as_ref())
+    }
+}
+
+use std::{convert::From, error::Error, fmt};
+
+#[derive(Debug)] // Allow the use of "{:?}" format specifier
+enum StoreError {
+    Read,
+}
+
+// Allow the use of "{}" format specifier
+impl fmt::Display for StoreError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            StoreError::Read => write!(f, "Read Error!",),
+        }
+    }
+}
+
+// Allow this type to be treated like an error
+impl Error for StoreError {
+    fn description(&self) -> &str {
+        match *self {
+            StoreError::Read => "Read failed!",
+        }
+    }
+
+    fn cause(&self) -> Option<&Error> {
+        match *self {
+            StoreError::Read => None,
+        }
     }
 }
