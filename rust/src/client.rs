9ed5e597364cde083d942c779d515bf5e32fc08f
finiteprods <kwok.doc@gmail.com>
2020-04-23 08:39:01 +0200

    summer skeleton

--- rust/src/client.rs
+++ rust/src/client.rs
@@ -1,45 +1,68 @@
 use crate::service::Handle;
-use crate::participant::Participant;
+use crate::participant::{Participant, Task};
+use sodiumoxide::crypto::box_;
+use std::sync::Arc;
 
 /// TODO
 pub struct Client {
     handle: Handle,
-    participant: Participant,
+    particip: Participant,
+    coord_encr_pk: Option<box_::PublicKey>,
 }
 
 impl Client {
     /// Create a new `Client`
     pub fn new() -> Self {
         let (handle, _events) = Handle::new();
-        let participant = Participant::new();
-        Self {
-            handle,
-            participant,
+        match Participant::new() {
+            Err(err) => panic!(err),
+            Ok(particip) => Self {
+                handle,
+                particip,
+                coord_encr_pk: None,
+            }
         }
     }
 
     /// TODO
     pub fn start(&mut self) {
-        self.pre_round()
+        // self.pre_round()
     }
 
-    fn pre_round(&mut self) {
-        // TODO
-        match self.handle.get_round_parameters().await {
-            Some(round_params) => panic!(),
-            None => panic!(),
+    async fn pre_round(&mut self) -> Option<()> {
+        let round_params = self.handle
+            .get_round_parameters()
+            .await?;
+        self.coord_encr_pk = Some(round_params.encr_pk);
+        let round_seed: &[u8] = round_params.seed.as_slice();
+        self.particip
+            .compute_signatures(round_seed);
+        let (sum_frac, upd_frac) = (round_params.sum, round_params.update);
+        match self.particip.check_task(sum_frac, upd_frac) {
+            Task::Sum    => self.summer().await,
+            Task::Update => self.updater(),
+            Task::None   => self.unselected(),
         }
     }
 
-    fn unselected(&mut self) {
-        // TODO
+    fn unselected(&mut self) -> Option<()> {
+        Some(())
     }
 
-    fn summer(&mut self) {
-        // TODO
+    async fn summer(&mut self) -> Option<()> {
+        let sum1_msg: Vec<u8> = self.particip
+            .compose_message_sum(&self.coord_encr_pk?);
+        self.handle
+            .send_message(sum1_msg)
+            .await;
+        let _pk = self.particip.get_encr_pk();
+        let _seed_dict: Arc<Vec<u8>> = self.handle
+            .get_seed_dict() // pass pk
+            .await?;
+        Some(())
     }
 
-    fn updater(&mut self) {
-        // TODO
+    fn updater(&mut self) -> Option<()> {
+        Some(())
     }
 }
