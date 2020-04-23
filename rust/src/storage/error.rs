e9c942ebf654624ab314b85da3cd7babcfd94e24
little-dude <corentinhenry@gmail.com>
2020-04-23 10:50:07 +0200

    derive Error for StoreError

--- rust/src/storage/error.rs
+++ rust/src/storage/error.rs
@@ -1,34 +1,9 @@
-use std::{error::Error, fmt};
+use thiserror::Error;
 
-#[derive(Debug)]
+#[derive(Debug, Error)]
 pub enum StoreError {
+    #[error("Read error!")]
     Read,
+    #[error("Convert error!")]
     Convert,
-}
-
-// Allow the use of "{}" format specifier
-impl fmt::Display for StoreError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match *self {
-            StoreError::Read => write!(f, "Read Error!",),
-            StoreError::Convert => write!(f, "Convert Error!",),
-        }
-    }
-}
-
-// Allow this type to be treated like an error
-impl Error for StoreError {
-    fn description(&self) -> &str {
-        match *self {
-            StoreError::Read => "Read failed!",
-            StoreError::Convert => "Convert failed!",
-        }
-    }
-
-    fn cause(&self) -> Option<&Error> {
-        match *self {
-            StoreError::Read => None,
-            StoreError::Convert => None,
-        }
-    }
 }
