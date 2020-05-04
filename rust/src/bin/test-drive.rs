ecfe4b0820050a0d7f7aef7db3807441e3e447be
finiteprods <kwok.doc@gmail.com>
2020-05-04 16:11:04 +0200

    remove spurious comments

--- rust/src/bin/test-drive.rs
+++ rust/src/bin/test-drive.rs
@@ -4,21 +4,6 @@ use xain_fl::service::Service;
 
 #[tokio::main]
 async fn main() -> Result<(), ClientError> {
-    // create a largeish number of clients
-    // do this in a loop: in each iteration,
-    // create a client c; spawn a task that runs c.per_round
-    // wait for them to finish
-    // if they all returned () ... that's as good as it gets for now
-    // error case easier to test: spawn too few
-    // "inbetween": dropouts
-
-    // tweak probabilities before actually running this
-    // print statements somehow would be nice
-    // later on it would be good to actually print a value to prove it's working
-    // e.g the global model
-
-//    let mut c = Client::new(1)?;
-//    let jh = tokio::spawn(c.per_round());
 
     let (_svc, hdl) = Service::new().unwrap();
 
