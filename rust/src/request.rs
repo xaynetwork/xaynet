f0c6cf12574e4d6a0a91ebf4e23daabbef515882
finiteprods <kwok.doc@gmail.com>
2020-06-09 10:18:43 +0200

    req paths; updated test driver

--- rust/src/request.rs
+++ rust/src/request.rs
@@ -1,6 +1,6 @@
 use crate::{crypto::ByteObject, ParticipantPublicKey};
 use bytes::{Buf, Bytes};
-use reqwest::{Client, Error, Response, StatusCode};
+use reqwest::{Client, Error, Response, StatusCode, Url};
 
 pub struct ClientReq {
     client: Client,
@@ -16,21 +16,23 @@ impl ClientReq {
     }
 
     pub async fn post_message(&self, msg: Vec<u8>) -> Result<StatusCode, Error> {
-        let response = self.client.post(self.address).body(msg).send().await?;
+        let url = format!("{}/{}", self.address, "message");
+        let response = self.client.post(&url).body(msg).send().await?;
         Ok(response.status())
     }
 
     pub async fn get_sums(&self) -> Result<Vec<u8>, Error> {
-        // TODO append path
-        let response = self.client.get(self.address).send().await?;
+        let url = format!("{}/{}", self.address, "sums");
+        let response = self.client.get(&url).send().await?;
         let bytes = response.bytes().await?;
         Ok(bytes.to_vec())
     }
 
     pub async fn get_seeds(&self, pk: ParticipantPublicKey) -> Result<Vec<u8>, Error> {
+        let url = format!("{}/{}", self.address, "seeds");
         let response = self
             .client
-            .get(self.address)
+            .get(&url)
             .header("Content-Type", "application/octet-stream")
             .body(pk.as_slice().to_vec())
             .send()
@@ -40,7 +42,8 @@ impl ClientReq {
     }
 
     pub async fn get_params(&self) -> Result<Vec<u8>, Error> {
-        let response = self.client.get(self.address).send().await?;
+        let url = format!("{}/{}", self.address, "params");
+        let response = self.client.get(&url).send().await?;
         let bytes = response.bytes().await?;
         Ok(bytes.to_vec())
     }
