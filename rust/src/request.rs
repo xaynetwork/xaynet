1536fdf273c42203ed1bcf76327e1580e14d17c2
finiteprods <kwok.doc@gmail.com>
2020-06-09 09:04:30 +0200

    request and rest server changes

--- rust/src/request.rs
+++ rust/src/request.rs
@@ -1,24 +1,47 @@
-use reqwest::Client;
-use reqwest::Response;
-use reqwest::Error;
+use crate::{crypto::ByteObject, ParticipantPublicKey};
+use bytes::{Buf, Bytes};
+use reqwest::{Client, Error, Response, StatusCode};
 
-pub struct PetHttp {
+pub struct ClientReq {
     client: Client,
     address: &'static str,
 }
 
-impl PetHttp {
-
+impl ClientReq {
     pub fn new(address: &'static str) -> Self {
-        PetHttp {
+        Self {
             client: Client::new(),
             address,
         }
     }
 
-    pub async fn get_sums(&self) {
+    pub async fn post_message(&self, msg: Vec<u8>) -> Result<StatusCode, Error> {
+        let response = self.client.post(self.address).body(msg).send().await?;
+        Ok(response.status())
+    }
+
+    pub async fn get_sums(&self) -> Result<Vec<u8>, Error> {
         // TODO append path
-        let response: Response = self.client.get(self.address).send().await?;
-        let _bytes = response.bytes().await?;
+        let response = self.client.get(self.address).send().await?;
+        let bytes = response.bytes().await?;
+        Ok(bytes.to_vec())
+    }
+
+    pub async fn get_seeds(&self, pk: ParticipantPublicKey) -> Result<Vec<u8>, Error> {
+        let response = self
+            .client
+            .get(self.address)
+            .header("Content-Type", "application/octet-stream")
+            .body(pk.as_slice().to_vec())
+            .send()
+            .await?;
+        let bytes = response.bytes().await?;
+        Ok(bytes.to_vec())
+    }
+
+    pub async fn get_params(&self) -> Result<Vec<u8>, Error> {
+        let response = self.client.get(self.address).send().await?;
+        let bytes = response.bytes().await?;
+        Ok(bytes.to_vec())
     }
 }
--- rust/src/rest.rs
+++ rust/src/rest.rs
@@ -18,12 +18,12 @@ pub async fn serve(addr: impl Into<SocketAddr> + 'static, handle: Handle) {
         .and(warp::post())
         .and(warp::body::bytes())
         .and(with_hdl(handle.clone()))
-        .and_then(send_message);
+        .and_then(handle_message);
 
     let sum_dict = warp::path!("sums")
         .and(warp::get())
         .and(with_hdl(handle.clone()))
-        .and_then(get_sums);
+        .and_then(handle_sums);
 
     // can't take pk as param in the path, as it doesn't impl FromStr...
     let seed_dict = warp::path!("seeds")
@@ -32,12 +32,12 @@ pub async fn serve(addr: impl Into<SocketAddr> + 'static, handle: Handle) {
         // it's not good HTTP 1.1 practice but what can you do
         .and(warp::body::bytes())
         .and(with_hdl(handle.clone()))
-        .and_then(get_seeds);
+        .and_then(handle_seeds);
 
     let round_params = warp::path!("params")
         .and(warp::get())
         .and(with_hdl(handle))
-        .and_then(get_params);
+        .and_then(handle_params);
 
     let routes = message
         .or(sum_dict)
@@ -47,32 +47,34 @@ pub async fn serve(addr: impl Into<SocketAddr> + 'static, handle: Handle) {
     warp::serve(routes).run(addr).await
 }
 
-async fn send_message(body: Bytes, handle: Handle) -> Result<impl warp::Reply, Infallible> {
+async fn handle_message(body: Bytes, handle: Handle) -> Result<impl warp::Reply, Infallible> {
     handle.send_message(body.to_vec()).await;
     Ok(warp::reply())
 }
 
-async fn get_sums(handle: Handle) -> Result<impl warp::Reply, Rejection> {
+async fn handle_sums(handle: Handle) -> Result<impl warp::Reply, Rejection> {
     let sum_dict = handle
         .get_sum_dict()
         .await
         .ok_or(warp::reject::not_found())?;
-    let sum_dict_bytes = Arc::try_unwrap(sum_dict).unwrap();
+    let sum_dict_bytes = Arc::try_unwrap(sum_dict).unwrap(); // HACK
     Ok(warp::reply::with_header(sum_dict_bytes, "Content-Type", "application/octet-stream"))
 }
 
 // TODO prob want Rejection rather than Infallible since body may not be pk
-async fn get_seeds(body: Bytes, handle: Handle) -> Result<impl warp::Reply, Infallible> {
+async fn handle_seeds(body: Bytes, handle: Handle) -> Result<impl warp::Reply, Rejection> {
     // TODO check key higher up and reject if cannot parse
-    let pk = ParticipantPublicKey::from_slice(body.bytes()).unwrap();
-    let seed_dict: Arc<_> = handle.get_seed_dict(pk).await.unwrap();
+    let pk = ParticipantPublicKey::from_slice(body.bytes()).ok_or(warp::reject::reject())?;
+    let seed_dict: Arc<_> = handle.get_seed_dict(pk).await.ok_or(warp::reject::not_found())?;
     let seed_dict_bytes = Arc::try_unwrap(seed_dict).unwrap();
     Ok(warp::reply::with_header(seed_dict_bytes, "Content-Type", "application/octet-stream"))
 }
 
-async fn get_params(handle: Handle) -> Result<impl warp::Reply, Infallible> {
-    handle.get_round_parameters().await;
-    Ok(warp::reply()) // TODO
+async fn handle_params(handle: Handle) -> Result<impl warp::Reply, Rejection> {
+    let _round_params = handle.get_round_parameters().await.ok_or(warp::reject::not_found())?;
+    //let round_params_bytes = Arc::try_unwrap(round_params).unwrap();
+    //Ok(warp::reply::with_header(round_params_bytes, "Content-Type", "application/octet-stream"));
+    Ok(warp::reply())
 }
 
 fn with_hdl(hdl: Handle) -> impl Filter<Extract = (Handle,), Error = Infallible> + Clone {
