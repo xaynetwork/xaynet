3e9c89c7288732a1ca4a5455bee316bf0dc0b4c8
Ricardo Saffi Marques <rsaffi@rsaffi.com>
2020-06-19 11:15:20 +0200

    PB-806 Add build-essential to list of dependencies that are installed -  was failing to compile

--- docker/install_rust.sh
+++ docker/install_rust.sh
@@ -2,14 +2,14 @@
 
 set -eux
 
-apt update
 apt install -y --no-install-recommends \
         ca-certificates \
         gcc \
         libc6-dev \
         wget \
         libssl-dev \
-        pkg-config
+        pkg-config \
+        build-essential
 
 wget "https://static.rust-lang.org/rustup/dist/x86_64-unknown-linux-gnu/rustup-init"
 chmod +x rustup-init
