af566fe8ade67beab84d8dcca9d9a05ef00c2bae
Jan Petsche <58227040+janpetschexain@users.noreply.github.com>
2020-06-30 16:07:32 +0200

    PB-871 add module docs

--- rust/src/message/mod.rs
+++ rust/src/message/mod.rs
@@ -1,4 +1,36 @@
-//! Messages.
+//! The messages of the PET protocol.
+//!
+//! The [`Message`]s adhere to the common [`Header`] and [`Payload`] scheme. They are parsed via
+//! [`MessageBuffer`]s which provide a checked access to the raw message, whereas the serialization
+//! and deserialization is implemented via the [`ToBytes`] and [`FromBytes`] traits.
+//!
+//! All messages are signed and encrypted in an authenticated encryption scheme via [`MessageSeal`]
+//! and [`MessageOpen`], which employ the wrappers of the [crypto module].
+//!
+//! # The sum message
+//! The [`Sum`] message is an abstraction for the values which a sum participant communicates to
+//! XayNet during the sum phase of the PET protocol. It contains the following values:
+//! - The sum signature proves the eligibility of the participant for the sum task.
+//! - The ephemeral public key is used by update participants to encrypt mask seeds in the update
+//!   phase for the process of mask aggregation in the sum2 phase.
+//!
+//! # The update message
+//! The [`Update`] message is an abstraction for the values which an update participant communicates
+//! to XayNet during the update phase of the PET protocol. It contains the following values:
+//! - The sum signature proves the ineligibility of the participant for the sum task.
+//! - The update signature proves the eligibility of the participant for the update task.
+//! - The masked model is the encrypted local update to the global model, which is trained on the
+//!   local data of the update participant.
+//! - The local seed dictionary stores the encrypted mask seed, which generates the local mask for
+//!   the local model, which is encrypted by the ephemeral public keys of the sum participants.
+//!
+//! # The sum2 message
+//! The [`Sum2`] message is an abstraction for the values which a sum participant communicates to
+//! XayNet during the sum2 phase of the PET protocol. It contains the following values:
+//! - The sum signature proves the eligibility of the participant for the sum task.
+//! - The global mask is used by XayNet to unmask the aggregated global model.
+//!
+//! [crypto module]: ../crypto/index.html
 
 pub(crate) mod buffer;
 pub(crate) mod header;
