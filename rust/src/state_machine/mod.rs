0255b11558e55fec6a56e0c7a3d229624cebd6ff
Robert Steiner <robertt.debug@gmail.com>
2020-07-03 09:38:55 +0200

    fix doc test

--- rust/src/state_machine/mod.rs
+++ rust/src/state_machine/mod.rs
@@ -179,7 +179,7 @@ where
     /// </pre></div>
     ///
     /// For example:
-    /// ```ignore
+    /// ```compile_fail
     /// sodiumoxide::init().unwrap();
     /// let state_machine =
     ///     StateMachine::from(PhaseState::<R, Idle>::new(coordinator_state, req_receiver));
