3cedc2dd7d98d3991d0d745bef6d97d47f7f460e
Jan Petsche <58227040+janpetschexain@users.noreply.github.com>
2020-07-15 11:11:58 +0200

    add sync client model api

--- rust/src/sdk/api_sync.rs
+++ rust/src/sdk/api_sync.rs
@@ -1,6 +1,15 @@
-use std::{ffi::CStr, os::raw::c_char, panic, ptr};
+use std::{
+    ffi::CStr,
+    os::raw::{c_char, c_int, c_uint, c_void},
+    panic,
+    ptr,
+    slice,
+};
 
-use crate::client::SyncClient;
+use crate::{
+    client::SyncClient,
+    mask::model::{FromPrimitives, IntoPrimitives, Model},
+};
 
 pub unsafe extern "C" fn new_client(address: *const c_char) -> *mut SyncClient {
     if address.is_null() {
@@ -40,5 +49,147 @@ pub unsafe extern "C" fn stop_client(client: *mut SyncClient) {
 pub unsafe extern "C" fn drop_client(client: *mut SyncClient) {
     if !client.is_null() {
         Box::from_raw(client);
+    }
+}
+
+// error codes:
+// -1: null pointers
+// 0: success
+// 1: no model available
+// 2: conversion failed
+pub unsafe extern "C" fn get_model(
+    client: *mut SyncClient,
+    dtype: c_uint,
+    model: *mut c_void,
+    len: c_uint,
+) -> c_int {
+    if client.is_null() || model.is_null() {
+        return -1_i32 as c_int;
+    }
+    let client = &mut *client;
+
+    let len = len as usize;
+    if let Some(m) = client.get_global_model() {
+        match dtype as u32 {
+            1 => {
+                // safety checks missing
+                let model = slice::from_raw_parts_mut(model as *mut f32, len);
+                for (i, p) in m.into_primitives().enumerate() {
+                    if i >= len {
+                        return 2_i32 as c_int;
+                    }
+                    if let Ok(p) = p {
+                        model[i] = p;
+                    } else {
+                        return 2_i32 as c_int;
+                    }
+                }
+            }
+            2 => {
+                let model = slice::from_raw_parts_mut(model as *mut f64, len);
+                for (i, p) in m.into_primitives().enumerate() {
+                    if i >= len {
+                        return 2_i32 as c_int;
+                    }
+                    if let Ok(p) = p {
+                        model[i] = p;
+                    } else {
+                        return 2_i32 as c_int;
+                    }
+                }
+            }
+            3 => {
+                let model = slice::from_raw_parts_mut(model as *mut i32, len);
+                for (i, p) in m.into_primitives().enumerate() {
+                    if i >= len {
+                        return 2_i32 as c_int;
+                    }
+                    if let Ok(p) = p {
+                        model[i] = p;
+                    } else {
+                        return 2_i32 as c_int;
+                    }
+                }
+            }
+            4 => {
+                let model = slice::from_raw_parts_mut(model as *mut i64, len);
+                for (i, p) in m.into_primitives().enumerate() {
+                    if i >= len {
+                        return 2_i32 as c_int;
+                    }
+                    if let Ok(p) = p {
+                        model[i] = p;
+                    } else {
+                        return 2_i32 as c_int;
+                    }
+                }
+            }
+            _ => return 2_i32 as c_int,
+        }
+        return 0_i32 as c_int;
+    } else {
+        return 1_i32 as c_int;
+    }
+}
+
+// error codes:
+// -1: null pointers
+// 0: success
+// 1: length mismatch
+// 2: conversion failed
+pub unsafe extern "C" fn update_model(
+    client: *mut SyncClient,
+    dtype: c_uint,
+    model: *const c_void,
+    len: c_uint,
+) -> c_int {
+    if client.is_null() || model.is_null() {
+        return -1_i32 as c_uint;
+    }
+    let client = &mut *client;
+
+    // length checks missing
+    let len = len as usize;
+    match dtype as u32 {
+        1 => {
+            // safety checks missing
+            let model = slice::from_raw_parts(model as *const f32, len);
+            if let Ok(m) = Model::from_primitives(model.iter().copied()) {
+                client.set_local_model(m);
+                return 0_i32 as c_int;
+            } else {
+                return 2_i32 as c_int;
+            }
+        }
+        2 => {
+            let model = slice::from_raw_parts(model as *const f64, len);
+            if let Ok(m) = Model::from_primitives(model.iter().copied()) {
+                client.set_local_model(m);
+                return 0_i32 as c_int;
+            } else {
+                return 2_i32 as c_int;
+            }
+        }
+        3 => {
+            let model = slice::from_raw_parts(model as *const i32, len);
+            if let Ok(m) = Model::from_primitives(model.iter().copied()) {
+                client.set_local_model(m);
+                return 0_i32 as c_int;
+            } else {
+                return 2_i32 as c_int;
+            }
+        }
+        4 => {
+            let model = slice::from_raw_parts(model as *const i64, len);
+            if let Ok(m) = Model::from_primitives(model.iter().copied()) {
+                client.set_local_model(m);
+                return 0_i32 as c_int;
+            } else {
+                return 2_i32 as c_int;
+            }
+        }
+        _ => {
+            return 2_i32 as c_int;
+        }
     }
 }
