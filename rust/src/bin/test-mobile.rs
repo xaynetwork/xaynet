8ea9aedf4486dc07b85bf5a0e5face418ae96af4
Robert Debug <robertt.debug@gmail.com>
2020-07-20 20:51:26 +0200

    Fix clippy warnings

--- rust/src/bin/test-mobile.rs
+++ rust/src/bin/test-mobile.rs
@@ -33,9 +33,9 @@ struct Opt {
 
 fn pause() {
     let mut stdout = stdout();
-    stdout.write(b"Press Enter to continue...").unwrap();
+    stdout.write_all(b"Press Enter to continue...").unwrap();
     stdout.flush().unwrap();
-    stdin().read(&mut [0]).unwrap();
+    stdin().read_exact(&mut [0]).unwrap();
 }
 
 fn main() -> Result<(), ()> {
@@ -60,7 +60,7 @@ fn main() -> Result<(), ()> {
         certificate: Certificate::new(),
     };
 
-    let mut client = MobileClient::new("http://localhost:8081", participant_settings.into());
+    let mut client = MobileClient::new("http://localhost:8081", participant_settings);
 
     let model = Model::from_primitives(vec![1; 4].into_iter()).unwrap();
 
