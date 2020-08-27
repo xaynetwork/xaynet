301c1129655648a04207bdda86d1d52bd5b9070b
Robert Debug <robertt.debug@gmail.com>
2020-08-27 10:28:02 +0200

    add error handling

--- rust/xaynet-client/src/mobile_client/mod.rs
+++ rust/xaynet-client/src/mobile_client/mod.rs
@@ -2,16 +2,35 @@ pub mod client;
 pub mod participant;
 
 use crate::{
-    api::HttpApiClient,
+    api::{ApiClient, HttpApiClient, HttpApiClientError},
     mobile_client::{
-        client::{get_global_model, ClientStateMachine, LocalModel},
+        client::{ClientStateMachine, LocalModel},
         participant::ParticipantSettings,
     },
 };
+use thiserror::Error;
 use xaynet_core::{
     crypto::{SecretSigningKey, SigningKeyPair},
     mask::Model,
+    InitError,
 };
+
+#[derive(Debug, Error)]
+/// Mobile client errors
+pub enum MobileClientError {
+    #[error("failed to deserialize mobile client: {0}")]
+    /// Failed to deserialize mobile client.
+    Deserialize(#[from] bincode::Error),
+    #[error("failed to initialize crypto module: {0}")]
+    /// Failed to initialize crypto module.
+    Init(#[from] InitError),
+    #[error("failed to initialize runtime: {0}")]
+    /// Failed to initialize runtime.
+    Runtime(#[from] std::io::Error),
+    #[error("API request failed: {0}")]
+    /// API request failed.
+    Api(#[from] HttpApiClientError),
+}
 
 pub struct MobileClient {
     api: HttpApiClient,
@@ -20,14 +39,23 @@ pub struct MobileClient {
 }
 
 impl MobileClient {
-    pub fn init(url: &str, participant_settings: ParticipantSettings) -> Self {
-        let client_state = ClientStateMachine::new(participant_settings).unwrap();
-        Self::new(url, client_state)
+    pub fn init(
+        url: &str,
+        participant_settings: ParticipantSettings,
+    ) -> Result<Self, MobileClientError> {
+        // It is critical that the initialization of sodiumoxide is successful.
+        // We'd better not run the client than having a broken crypto.
+        //
+        // Refs:
+        // https://doc.libsodium.org/usage
+        // https://github.com/jedisct1/libsodium/issues/908
+        let client_state = ClientStateMachine::new(participant_settings)?;
+        Ok(Self::new(url, client_state))
     }
 
-    pub fn deserialize(url: &str, bytes: &[u8]) -> Self {
-        let client_state: ClientStateMachine = bincode::deserialize(bytes).unwrap();
-        Self::new(url, client_state)
+    pub fn restore(url: &str, bytes: &[u8]) -> Result<Self, MobileClientError> {
+        let client_state: ClientStateMachine = bincode::deserialize(bytes)?;
+        Ok(Self::new(url, client_state))
     }
 
     fn new(url: &str, client_state: ClientStateMachine) -> Self {
@@ -41,14 +69,33 @@ impl MobileClient {
     }
 
     pub fn serialize(&self) -> Vec<u8> {
+        // Safe to unwrap:
+        //
+        // - all sequences have known length
+        //   - an iterator is an example for a sequence with an unknown length
+        // - no untagged enum
+        //
+        // Refs:
+        // - https://github.com/servo/bincode/issues/293
+        // - https://github.com/servo/bincode/issues/255
+        // - https://github.com/servo/bincode/issues/130#issuecomment-284641263
         bincode::serialize(&self.client_state).unwrap()
     }
 
-    pub fn get_global_model(&mut self) -> Option<Model> {
-        Self::runtime().block_on(async { get_global_model(&mut self.api).await })
+    pub fn get_global_model(&mut self) -> Result<Option<Model>, MobileClientError> {
+        Self::runtime()?
+            .block_on(async { self.api.get_model().await })
+            .map_err(|err| err.into())
     }
 
-    pub fn perform_task(self) -> Self {
+    pub fn try_to_proceed(self) -> Result<Self, (Self, MobileClientError)> {
+        let mut runtime = match Self::runtime() {
+            Ok(runtime) => runtime,
+            // We don't want to loose the current client because of a runtime error.
+            // Therefore we return the error as well as the current client.
+            Err(err) => return Err((self, err.into())),
+        };
+
         let MobileClient {
             mut api,
             mut local_model,
@@ -56,15 +103,21 @@ impl MobileClient {
         } = self;
 
         let client_state =
-            Self::runtime().block_on(async { client_state.next(&mut api, &mut local_model).await });
+            runtime.block_on(async { client_state.next(&mut api, &mut local_model).await });
 
-        Self {
+        Ok(Self {
             api,
             local_model,
             client_state,
-        }
+        })
     }
 
+    /// Sets the local model.
+    ///
+    /// The local model is only sent if the client has been selected as an update client.
+    /// If the client is an update client and no local model is available, the client remains
+    /// in this state until a local model has been set or a new round has been started by the
+    /// coordinator.
     pub fn set_local_model(&mut self, model: Model) {
         self.local_model.set_local_model(model);
     }
@@ -74,12 +127,14 @@ impl MobileClient {
         secret
     }
 
-    fn runtime() -> tokio::runtime::Runtime {
+    fn runtime() -> Result<tokio::runtime::Runtime, std::io::Error> {
+        // Following the code of tokio, the creation of the I/O driver can result in an error.
+        // It is not documented what exact condition can cause an error. Therefore we don't unwrap
+        // here.
         tokio::runtime::Builder::new()
             .basic_scheduler()
             .enable_all()
             .build()
-            .unwrap()
     }
 }
 
