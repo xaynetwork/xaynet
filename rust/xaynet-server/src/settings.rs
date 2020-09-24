f2c0608a9b0a35690877da37f0f20d430b370476
Jan Petsche <58227040+janpetschexain@users.noreply.github.com>
2020-09-24 18:09:54 +0200

    add settings tests

--- rust/xaynet-server/src/settings.rs
+++ rust/xaynet-server/src/settings.rs
@@ -629,3 +629,79 @@ impl From<TlsSettings> for Option<(PathBuf, PathBuf)> {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_settings_new() {
+        assert!(Settings::new(PathBuf::from("../../configs/config.toml")).is_ok());
+        assert!(Settings::new(PathBuf::from("")).is_err());
+    }
+
+    #[test]
+    fn test_validate_pet() {
+        assert!(validate_pet(&PetSettings::default()).is_ok());
+
+        // phase times
+        assert!(validate_pet(&PetSettings {
+            min_sum_time: 2,
+            max_sum_time: 1,
+            ..PetSettings::default()
+        })
+        .is_err());
+        assert!(validate_pet(&PetSettings {
+            min_update_time: 2,
+            max_update_time: 1,
+            ..PetSettings::default()
+        })
+        .is_err());
+
+        // fractions
+        assert!(validate_pet(&PetSettings {
+            sum: 0.,
+            ..PetSettings::default()
+        })
+        .is_err());
+        assert!(validate_pet(&PetSettings {
+            sum: 1.,
+            ..PetSettings::default()
+        })
+        .is_err());
+        assert!(validate_pet(&PetSettings {
+            update: 0.,
+            ..PetSettings::default()
+        })
+        .is_err());
+        assert!(validate_pet(&PetSettings {
+            update: 1.,
+            ..PetSettings::default()
+        })
+        .is_err());
+    }
+
+    #[test]
+    fn test_validate_tls() {
+        assert!(validate_tls(&TlsSettings {
+            cert: Some(String::new()),
+            key: Some(String::new())
+        })
+        .is_ok());
+        assert!(validate_tls(&TlsSettings {
+            cert: None,
+            key: None
+        })
+        .is_ok());
+        assert!(validate_tls(&TlsSettings {
+            cert: Some(String::new()),
+            key: None
+        })
+        .is_err());
+        assert!(validate_tls(&TlsSettings {
+            cert: None,
+            key: Some(String::new())
+        })
+        .is_err());
+    }
+}
