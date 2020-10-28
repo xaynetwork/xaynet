f537360419625c6cc6b8f8b74dedd35f23858ee5
Anselmo Sampietro <anselmo.sampietro@xain.io>
2020-10-28 12:20:30 +0100

    XN-1190 fix format and warnings

--- rust/benches/benches/models/serialize_models.rs
+++ rust/benches/benches/models/serialize_models.rs
@@ -2,32 +2,35 @@
 
 mod utils;
 
-use std::time::Duration;
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use std::time::Duration;
 
-use xaynet_core::mask::{Model, IntoPrimitives};
+use xaynet_core::mask::{IntoPrimitives, Model};
 
 fn serialize(model: &Model) -> Vec<i32> {
-    model.clone().into_primitives_unchecked().collect::<Vec<i32>>()
+    model
+        .clone()
+        .into_primitives_unchecked()
+        .collect::<Vec<i32>>()
 }
 
 pub fn serialize_4bytes_model(c: &mut Criterion) {
-    let model_4bytes = utils::serialize_or_parse_model::make_model_4bytes();
-    c.bench_function("serialize 4 bytes model to primitives", |bench| {
+    let model_4bytes = utils::vector_or_model::make_model_4bytes();
+    c.bench_function("serialize 4 bytes model into primitives", |bench| {
         bench.iter(|| serialize(black_box(&model_4bytes)))
     });
 }
 
 pub fn serialize_100kB_model(c: &mut Criterion) {
-    let model_100kB = utils::serialize_or_parse_model::make_model_100kB();
+    let model_100kB = utils::vector_or_model::make_model_100kB();
     c.bench_function("serialize 100kB model into primitives", |bench| {
         bench.iter(|| serialize(black_box(&model_100kB)))
     });
 }
 
 pub fn serialize_1MB_model(c: &mut Criterion) {
-    let model_1MB = utils::serialize_or_parse_model::make_model_1MB();
-    c.bench_function("serialize 1MB model to primitives", |bench| {
+    let model_1MB = utils::vector_or_model::make_model_1MB();
+    c.bench_function("serialize 1MB model into primitives", |bench| {
         bench.iter(|| serialize(black_box(&model_1MB)))
     });
 }
