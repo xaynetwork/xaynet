c772ac20fc4b84213c34c949184b9675af7fe577
Robert Debug <robertt.debug@gmail.com>
2020-11-26 15:36:59 +0100

    rename size to length in model settings

--- rust/xaynet-server/src/state_machine/coordinator.rs
+++ rust/xaynet-server/src/state_machine/coordinator.rs
@@ -51,7 +51,7 @@ impl CoordinatorState {
             update: pet_settings.update,
             seed: RoundSeed::zeroed(),
             mask_config: mask_config.clone().into(),
-            model_length: model_settings.size,
+            model_length: model_settings.length,
         };
         let round_id = 0;
         Self {
@@ -65,7 +65,7 @@ impl CoordinatorState {
             max_sum_time: pet_settings.max_sum_time,
             max_update_time: pet_settings.max_update_time,
             mask_config,
-            model_size: model_settings.size,
+            model_size: model_settings.length,
         }
     }
 }
