4992dc4a6f8dc52212cd30ca32a6bd235f1027a6
Anselmo Sampietro <anselmo.sampietro@xain.io>
2021-01-14 11:27:24 +0100

    add data points unit tests

--- rust/xaynet-analytics/src/data_combination/data_points/was_active_each_past_period.rs
+++ rust/xaynet-analytics/src/data_combination/data_points/was_active_each_past_period.rs
@@ -1,5 +1,5 @@
 use chrono::{DateTime, Utc};
-use std::collections::HashMap;
+use std::collections::BTreeMap;
 
 use crate::{
     data_combination::data_points::data_point::{
@@ -24,8 +24,8 @@ impl CalcWasActiveEachPastPeriod {
     }
 
     // TODO: this could possibly later be optimised by sorting events by timestamp and caching the last timestamp added to the HashMap
-    fn group_timestamps_by_period_threshold(&self) -> HashMap<DateTime<Utc>, Vec<DateTime<Utc>>> {
-        let mut timestamps_by_period_threshold = HashMap::new();
+    fn group_timestamps_by_period_threshold(&self) -> BTreeMap<DateTime<Utc>, Vec<DateTime<Utc>>> {
+        let mut timestamps_by_period_threshold = BTreeMap::new();
         for these_thresholds in self.period_thresholds.windows(2) {
             // safe unwrap: `windows` guarantees that there are at least two elements.
             // If `period_thresholds` contains less than two elements, this code block is not executed
@@ -52,9 +52,153 @@ impl CalculateDataPoints for CalcWasActiveEachPastPeriod {
 
     fn calculate(&self) -> Vec<u32> {
         let timestamps_by_period_threshold = self.group_timestamps_by_period_threshold();
+        // since we are travelling 'back in time' we need to reverse the order of the values of the BTreeMap
         timestamps_by_period_threshold
             .values()
+            .rev()
             .map(|timestamps| !timestamps.is_empty() as u32)
             .collect::<Vec<u32>>()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use chrono::{DateTime, Duration, Utc};
+
+    use crate::{
+        data_combination::data_points::{
+            data_point::{CalculateDataPoints, DataPointMetadata, Period, PeriodUnit},
+            was_active_each_past_period::CalcWasActiveEachPastPeriod,
+        },
+        data_provision::analytics_event::{AnalyticsEvent, AnalyticsEventType},
+    };
+
+    #[test]
+    fn test_calculate_no_events_in_a_period() {
+        let end_period = DateTime::parse_from_rfc3339("2021-02-02T00:00:00-00:00")
+            .unwrap()
+            .with_timezone(&Utc);
+        let metadata = DataPointMetadata::new(Period::new(PeriodUnit::Days, 1), end_period);
+        let period_thresholds = vec![end_period, end_period - Duration::days(1)];
+        let was_active_each_past_period =
+            CalcWasActiveEachPastPeriod::new(metadata, Vec::new(), period_thresholds);
+        assert_eq!(was_active_each_past_period.calculate(), vec![0]);
+    }
+
+    #[test]
+    fn test_calculate_one_event_in_a_period() {
+        let end_period = DateTime::parse_from_rfc3339("2021-03-03T00:00:00-00:00")
+            .unwrap()
+            .with_timezone(&Utc);
+        let metadata = DataPointMetadata::new(Period::new(PeriodUnit::Days, 1), end_period);
+        let events = vec![AnalyticsEvent::new(
+            "test1",
+            AnalyticsEventType::UserAction,
+            end_period - Duration::hours(12),
+            None,
+        )];
+        let period_thresholds = vec![end_period, end_period - Duration::days(1)];
+        let was_active_each_past_period =
+            CalcWasActiveEachPastPeriod::new(metadata, events, period_thresholds);
+        assert_eq!(was_active_each_past_period.calculate(), vec![1]);
+    }
+
+    #[test]
+    fn test_calculate_no_events_in_two_periods() {
+        let end_period = DateTime::parse_from_rfc3339("2021-04-04T00:00:00-00:00")
+            .unwrap()
+            .with_timezone(&Utc);
+        let metadata = DataPointMetadata::new(Period::new(PeriodUnit::Days, 2), end_period);
+        let period_thresholds = vec![
+            end_period,
+            end_period - Duration::days(1),
+            end_period - Duration::days(2),
+        ];
+        let was_active_each_past_period =
+            CalcWasActiveEachPastPeriod::new(metadata, Vec::new(), period_thresholds);
+        assert_eq!(was_active_each_past_period.calculate(), vec![0, 0]);
+    }
+
+    #[test]
+    fn test_calculate_one_event_in_one_period_zero_in_another() {
+        let end_period = DateTime::parse_from_rfc3339("2021-05-05T00:00:00-00:00")
+            .unwrap()
+            .with_timezone(&Utc);
+        let metadata = DataPointMetadata::new(Period::new(PeriodUnit::Days, 2), end_period);
+        let events = vec![AnalyticsEvent::new(
+            "test1",
+            AnalyticsEventType::UserAction,
+            end_period - Duration::hours(12),
+            None,
+        )];
+        let period_thresholds = vec![
+            end_period,
+            end_period - Duration::days(1),
+            end_period - Duration::days(2),
+        ];
+        let was_active_each_past_period =
+            CalcWasActiveEachPastPeriod::new(metadata, events, period_thresholds);
+        assert_eq!(was_active_each_past_period.calculate(), vec![1, 0]);
+    }
+
+    #[test]
+    fn test_calculate_two_events_in_one_period_zero_in_another() {
+        let end_period = DateTime::parse_from_rfc3339("2021-06-06T00:00:00-00:00")
+            .unwrap()
+            .with_timezone(&Utc);
+        let metadata = DataPointMetadata::new(Period::new(PeriodUnit::Days, 2), end_period);
+        let events = vec![
+            AnalyticsEvent::new(
+                "test1",
+                AnalyticsEventType::UserAction,
+                end_period - Duration::hours(12),
+                None,
+            ),
+            AnalyticsEvent::new(
+                "test2",
+                AnalyticsEventType::Error,
+                end_period - Duration::hours(15),
+                None,
+            ),
+        ];
+        // let period_thresholds = vec![end_period, end_period - Duration::days(1), end_period - Duration::days(2)];
+        let period_thresholds = vec![
+            end_period,
+            end_period - Duration::days(1),
+            end_period - Duration::days(2),
+        ];
+        let was_active_each_past_period =
+            CalcWasActiveEachPastPeriod::new(metadata, events, period_thresholds);
+        assert_eq!(was_active_each_past_period.calculate(), vec![1, 0]);
+    }
+
+    #[test]
+    fn test_calculate_two_periods_with_one_event_each() {
+        let end_period = DateTime::parse_from_rfc3339("2021-07-07T00:00:00-00:00")
+            .unwrap()
+            .with_timezone(&Utc);
+        let metadata = DataPointMetadata::new(Period::new(PeriodUnit::Days, 2), end_period);
+        let events = vec![
+            AnalyticsEvent::new(
+                "test1",
+                AnalyticsEventType::UserAction,
+                end_period - Duration::hours(12),
+                None,
+            ),
+            AnalyticsEvent::new(
+                "test2",
+                AnalyticsEventType::Error,
+                end_period - Duration::hours(36),
+                None,
+            ),
+        ];
+        let period_thresholds = vec![
+            end_period,
+            end_period - Duration::days(1),
+            end_period - Duration::days(2),
+        ];
+        let was_active_each_past_period =
+            CalcWasActiveEachPastPeriod::new(metadata, events, period_thresholds);
+        assert_eq!(was_active_each_past_period.calculate(), vec![1, 1]);
     }
 }
--- rust/xaynet-analytics/src/data_combination/data_points/was_active_past_n_days.rs
+++ rust/xaynet-analytics/src/data_combination/data_points/was_active_past_n_days.rs
@@ -22,3 +22,36 @@ impl CalculateDataPoints for CalcWasActivePastNDays {
         vec![!self.events.is_empty() as u32]
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use chrono::{Duration, Utc};
+
+    use crate::{
+        data_combination::data_points::{
+            data_point::{CalculateDataPoints, DataPointMetadata, Period, PeriodUnit},
+            was_active_past_n_days::CalcWasActivePastNDays,
+        },
+        data_provision::analytics_event::{AnalyticsEvent, AnalyticsEventType},
+    };
+
+    #[test]
+    fn test_calculate_without_events() {
+        let metadata = DataPointMetadata::new(Period::new(PeriodUnit::Days, 1), Utc::now());
+        let was_active_past_n_days = CalcWasActivePastNDays::new(metadata, Vec::new());
+        assert_eq!(was_active_past_n_days.calculate(), vec![0]);
+    }
+
+    #[test]
+    fn test_calculate_with_events() {
+        let metadata = DataPointMetadata::new(Period::new(PeriodUnit::Days, 1), Utc::now());
+        let events = vec![AnalyticsEvent::new(
+            "test1",
+            AnalyticsEventType::AppEvent,
+            metadata.end - Duration::hours(12),
+            None,
+        )];
+        let was_active_past_n_days = CalcWasActivePastNDays::new(metadata, events);
+        assert_eq!(was_active_past_n_days.calculate(), vec![1]);
+    }
+}
